e9871eec4d51c35caf347744513dcd7d1ca74536
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2021-03-27 13:07:54 -0400

    Time: 8 ms (42.37%), Space: 39.7 MB (29.07%) - LeetHub

---
+++ single-number/single-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Set<Integer> s = new HashSet<>();
+        for(int i = 0; i< nums.length;i++){
+            if(!s.contains(nums[i])){
+                s.add(nums[i]);
+            }
+            else{
+                s.remove(nums[i]);
+            }
+        }
+        
+        return s.iterator().next();
+        
+        
+    }
+}
