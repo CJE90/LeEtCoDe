e4a937fa72b4cf7b96cdc41d6cf4882a0792750d
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2021-04-29 17:58:14 -0400

    Time: 1 ms (25.48%), Space: 39 MB (8.53%) - LeetHub

---
+++ validate-binary-search-tree/validate-binary-search-tree.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        List<Integer> arr = new ArrayList<>();
+        if(root == null){
+            return false;
+        }
+        dfs(root, arr);
+        
+        for(int i = 0; i<arr.size() -1 ; i++){
+            if(arr.get(i) >= arr.get(i+1)){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public void dfs(TreeNode root, List<Integer> arr){
+        if(root == null){
+            return;
+        }
+        dfs(root.left, arr);
+        arr.add(root.val);
+        dfs(root.right, arr);
+        
+        return;
+    }
+}
