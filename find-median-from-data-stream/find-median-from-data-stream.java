3883ee28b1875d2228984bffa5a572563c787ca9
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2021-06-20 14:42:52 -0400

    Time: 47 ms (75.99%), Space: 50.4 MB (56.12%) - LeetHub

--- find-median-from-data-stream/find-median-from-data-stream.java
+++ find-median-from-data-stream/find-median-from-data-stream.java
@@ -1,25 +1,39 @@
 class MedianFinder {
-List<Integer> myArr = new ArrayList<>();
+    private PriorityQueue<Integer> lo = new PriorityQueue<>(Collections.reverseOrder());//max heap
+    private PriorityQueue<Integer> hi = new PriorityQueue<>(); //min heap
+    
+
     /** initialize your data structure here. */
     public MedianFinder() {
-         
+        
     }
     
     public void addNum(int num) {
-        myArr.add(num);
-        Collections.sort(myArr);
+        // Default putting nums in the Max heap
+        lo.offer(num);
+        // Need to verify that numbers in lo are <= to numbers in hi, if not move number from lo to hi
+        if(lo.size() != 0 && hi.size() != 0 && lo.peek() > hi.peek()){
+            hi.offer(lo.poll());
+        }
         
-        
+        // Verify sizes are balanced +/- 1
+        if(lo.size()>(hi.size()+1)){
+            hi.offer(lo.poll());
+        }
+        else if(hi.size()>lo.size()+1){
+            lo.offer(hi.poll());
+        }
     }
     
     public double findMedian() {
-        int middle = myArr.size()/2;
-        if(myArr.size() % 2 == 0){
-            return (myArr.get(middle)+myArr.get(middle-1))/2.0;
+        if(hi.size() > lo.size()){
+            return hi.peek();
+        }
+        else if(hi.size() < lo.size()){
+            return lo.peek();
         }
         else{
-            return myArr.get(middle);
-        }
+        return (lo.peek()+hi.peek())*0.5;}
         
     }
 }
@@ -30,5 +44,3 @@ List<Integer> myArr = new ArrayList<>();
  * obj.addNum(num);
  * double param_2 = obj.findMedian();
  */
-
-
