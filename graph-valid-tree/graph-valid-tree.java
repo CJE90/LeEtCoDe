16f047330d0f5f12970c46ded5cdd3084757144b
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2021-06-30 18:30:42 -0400

    Time: 0 ms (100.00%), Space: 39.8 MB (48.22%) - LeetHub

--- graph-valid-tree/graph-valid-tree.java
+++ graph-valid-tree/graph-valid-tree.java
@@ -1,33 +1,32 @@
 class Solution {
     public boolean validTree(int n, int[][] edges) {
-        if(n == 1){
-            return true;
+        int[] roots = new int[n];
+        int countOfComponents = n;
+        
+        for(int i = 0; i<n;i++){
+            roots[i] = i;
         }
-        int[] id = new int[n];
-        for(int i = 0; i < n; i++){
-            id[i] = i;
-        }
+        
         for(int[] edge:edges){
-            if(isConnected(edge[0],edge[1],id)){
+            if(connected(edge[0],edge[1],roots)){
                 return false;
             }
-            union(edge[0],edge[1], id);
-            n--;
+            roots[root(edge[1],roots)] = edge[0];
+            countOfComponents--;
         }
-        return n == 1;
+        return countOfComponents == 1;
+        
         
     }
-    public boolean isConnected(int p, int q, int[] id){
-        return root(p, id) == root(q, id);
-    }
-    public void union(int p, int q, int[] id){
-        id[root(q,id)] = root(p,id);
-    }
-    public int root(int i, int[] id){
-        while(i != id[i]){
-            id[i] = id[id[i]];
-            i = id[i];
+    public int root(int i, int[] roots){
+        while(i != roots[i]){
+            roots[i] = roots[roots[i]];
+            i = roots[i];
         }
         return i;
     }
+    public boolean connected(int p, int q, int[] roots){
+        return root(p, roots) == root(q,roots);
+    }
+    
 }
