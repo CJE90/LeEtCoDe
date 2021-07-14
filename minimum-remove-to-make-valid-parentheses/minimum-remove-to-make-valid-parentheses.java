140d5f782b7693b0e86c7443ec877eb3b995b259
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2021-07-13 20:14:37 -0400

    Time: 16 ms (73.22%), Space: 40.3 MB (31.33%) - LeetHub

--- minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
+++ minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -1,53 +1,27 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
+        StringBuilder str = new StringBuilder(s);
         Deque<Integer> stack = new ArrayDeque<>();
         
-        for(int i = 0; i < s.length(); i++){
+        for(int i = 0; i<s.length();i++){
             if(s.charAt(i) == '('){
                 stack.push(i);
             }
-            else if(s.charAt(i) == ')'){
+            if(s.charAt(i) == ')'){
                 if(stack.isEmpty()){
-                    stack.push(i);
+                    str.setCharAt(i, '*');
                 }
                 else{
-                    
-                    if(s.charAt(stack.peek()) == ')'){
-                        stack.push(i);
-                    }
-                    else{
-                        stack.pop();
-                    }
-                    
+                    stack.pop();
                 }
             }
         }
         
-        
-        int[] nums = new int[stack.size()];
-        int numsSize = stack.size();
-        for(int i = 0; i< numsSize; i++){
-            nums[i] = stack.pop();
+        while(!stack.isEmpty()){
+            str.setCharAt(stack.pop(), '*');
         }
-            
-        int count = nums.length-1;
         
-        StringBuilder str = new StringBuilder();
-        
-        for(int i = 0; i<s.length();i++){
-            if(count >= 0){
-                if(i == nums[count]){
-                    count--;
-                    continue;
-                }
-                else{
-                    str.append(s.charAt(i));
-                }  
-            }
-            else{
-            str.append(s.charAt(i));}
-        }
-        return str.toString();
-        
+        return str.toString().replaceAll("\\*","");
+
     }
 }
