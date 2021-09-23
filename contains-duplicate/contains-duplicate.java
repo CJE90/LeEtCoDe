5e152760e2e71fef853f2b4228e197454edd0987
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2021-09-23 19:24:15 -0400

    Time: 5 ms (80.51%), Space: 43.3 MB (77.27%) - LeetHub

---
+++ contains-duplicate/contains-duplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> hashSet = new HashSet<>();
+        for(Integer n:nums){
+            if(hashSet.contains(n)){
+                return true;
+            }
+            hashSet.add(n);
+        }
+        return false;
+    }
+}
