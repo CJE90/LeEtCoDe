b72dc7ca853ff51069dc82b253afb75fdbc7b7d7
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2021-10-04 21:09:50 -0400

    Time: 203 ms (21.44%), Space: 38.5 MB (48.82%) - LeetHub

--- word-search/word-search.java
+++ word-search/word-search.java
@@ -1,12 +1,10 @@
 class Solution {
-    private final int[][] direction = {{0,1},{0,-1},{1,0},{-1,0}};
+    private static int[][] directions = {{0,1},{0,-1},{1,0},{-1,0}};
     public boolean exist(char[][] board, String word) {
-        int rowLength = board.length;
-        int colLength = board[0].length;
         
-        for(int i = 0 ; i <rowLength; i++){
-            for(int j = 0 ; j < colLength; j++){
-                if(board[i][j] == word.charAt(0) && exists(board, i, j, 0, word)){
+        for(int i = 0; i<board.length; i++){
+            for(int j = 0; j<board[0].length;j++){
+                if(exists(board, i, j, 0, word)){
                     return true;
                 }
             }
@@ -14,26 +12,25 @@ class Solution {
         return false;
         
     }
-    
-    public boolean exists(char[][]board, int row, int col, int index, String word){
-        if(index>=word.length()){
+    public boolean exists(char[][] board, int row, int col, int index, String word){
+        if(index >= word.length()){
             return true;
         }
-        if(row<0 || row>= board.length || col<0 || col>= board[0].length || word.charAt(index) != board[row][col]){
+        
+        if(row<0 || row>=board.length || col<0 || col>=board[0].length || board[row][col] != word.charAt(index)){
             return false;
         }
         
+        boolean found = false;
         char temp = board[row][col];
         board[row][col] = '#';
         
-        boolean found = false;
-        
-        for(int[] d:direction){
-            if(exists(board, row+d[0], col+d[1], index+1, word)){
+        for(int[] dir:directions){
+            if(exists(board, row+dir[0], col+dir[1], index+1, word)){
                 found = true;
-                break;
             }
         }
+        
         board[row][col] = temp;
         return found;
     }
