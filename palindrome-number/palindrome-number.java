13aafc74e6a98055f47f0b71648729f3308ac142
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2021-10-08 21:39:53 -0400

    Time: 8 ms (55.20%), Space: 38.3 MB (80.73%) - LeetHub

---
+++ palindrome-number/palindrome-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        char[] digits = String.valueOf(x).toCharArray();
+        int l = 0; int r = digits.length-1;
+        while(l<=r){
+            if(digits[l]!= digits[r]){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+        
+    }
+}
