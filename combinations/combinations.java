0ce7b705e204529bb0747da313772c59852818b7
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2021-10-17 18:48:09 -0400

    Time: 1 ms (100.00%), Space: 40.3 MB (76.52%) - LeetHub

--- combinations/combinations.java
+++ combinations/combinations.java
@@ -9,28 +9,13 @@ class Solution {
             list.add(new ArrayList(tempList));
             return;
         }
-            for(int i = index; i<=n; i++){
+            for(int i = index; i<=n-k+1; i++){
                 tempList.add(i);
                 backtrack(list, tempList, n, k-1, i+1);
                 tempList.remove(tempList.size()-1);
             }  
     }
     
-	// public static List<List<Integer>> combine(int n, int k) {
-	// 	List<List<Integer>> combs = new ArrayList<List<Integer>>();
-	// 	combine(combs, new ArrayList<Integer>(), 1, n, k);
-	// 	return combs;
-	// }
-	// public static void combine(List<List<Integer>> combs, List<Integer> comb, int start, int n, int k) {
-	// 	if(k==0) {
-	// 		combs.add(new ArrayList<Integer>(comb));
-	// 		return;
-	// 	}
-	// 	for(int i=start;i<=n;i++) {
-	// 		comb.add(i);
-	// 		combine(combs, comb, i+1, n, k-1);
-	// 		comb.remove(comb.size()-1);
-	// 	}
-	// }
+	
 }
 
