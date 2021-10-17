62e9fa3a61f95f0e4cee6390fabc4ee24b33d8b9
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2021-10-17 12:25:26 -0400

    Time: 96 ms (73.32%), Space: 36.9 MB (84.15%) - LeetHub

--- word-search/word-search.java
+++ word-search/word-search.java
@@ -1,37 +1,36 @@
 class Solution {
-    private static int[][] directions = {{0,1},{0,-1},{1,0},{-1,0}};
     public boolean exist(char[][] board, String word) {
-        
-        for(int i = 0; i<board.length; i++){
-            for(int j = 0; j<board[0].length;j++){
-                if(exists(board, i, j, 0, word)){
+        int row = board.length;
+        int col = board[0].length;
+        for(int i = 0; i<row; i++){
+            for(int j = 0; j<col; j++){
+                if(board[i][j] == word.charAt(0) && backtrack(board, word, i, j, 0)){
                     return true;
                 }
             }
         }
-        return false;
-        
+        return false;  
     }
-    public boolean exists(char[][] board, int row, int col, int index, String word){
-        if(index >= word.length()){
+    public boolean backtrack(char[][] board, String word, int row, int col, int index){
+        if(index>=word.length()){
             return true;
         }
-        
-        if(row<0 || row>=board.length || col<0 || col>=board[0].length || board[row][col] != word.charAt(index)){
+        if(row < 0 || row >= board.length || col < 0 || col >= board[0].length || board[row][col] != word.charAt(index)){
             return false;
         }
         
-        boolean found = false;
-        char temp = board[row][col];
+        boolean foundAValidLetter = false;
+        char mark = word.charAt(index);
         board[row][col] = '#';
         
-        for(int[] dir:directions){
-            if(exists(board, row+dir[0], col+dir[1], index+1, word)){
-                found = true;
-            }
+        if(backtrack(board, word, row+1, col, index+1) ||
+           backtrack(board, word, row-1, col, index+1) ||
+           backtrack(board, word, row, col+1, index+1) ||
+           backtrack(board, word, row, col-1, index+1 )){
+             foundAValidLetter = true;
         }
-        
-        board[row][col] = temp;
-        return found;
+
+        board[row][col] = mark;
+        return foundAValidLetter;
     }
 }
