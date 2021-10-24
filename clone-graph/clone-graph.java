bd1716576cc09872f6cdb6d0d9f070fc0c94e30a
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2021-10-24 09:28:04 -0400

    Time: 37 ms (40.39%), Space: 40.5 MB (6.44%) - LeetHub

--- clone-graph/clone-graph.java
+++ clone-graph/clone-graph.java
@@ -19,27 +19,22 @@ class Node {
 */
 
 class Solution {
+    Map<Node, Node> visited = new HashMap<>();
     public Node cloneGraph(Node node) {
         if(node == null){
             return null;
         }
-        Queue<Node> que = new LinkedList<>();
-        Map<Node, Node> visited = new HashMap<>();
-        
-        que.offer(node);
-        visited.put(node, new Node(node.val, new ArrayList<>()));
-        
-        while(!que.isEmpty()){
-            Node n = que.poll();
-            for(Node neighbor:n.neighbors){
-                if(!visited.containsKey(neighbor)){
-                    que.add(neighbor);
-                    visited.put(neighbor, new Node (neighbor.val, new ArrayList<>()));
-                    
-                }
-                visited.get(n).neighbors.add(visited.get(neighbor));   
-            }
+        if(visited.containsKey(node)){
+            return visited.get(node);
         }
-        return visited.get(node); 
+        
+        Node clonedNode = new Node(node.val, new ArrayList<>());
+        visited.put(node, clonedNode);
+        for(Node neighbor:node.neighbors){
+            clonedNode.neighbors.add(cloneGraph(neighbor));
+        }
+        return clonedNode;
+        
+        
     }
 }
