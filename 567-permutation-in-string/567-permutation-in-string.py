c3e3024b8c751bdb380e32ac564c0a41e19f7993
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-01-27 21:28:16 -0500

    Time: 121 ms (47.15%), Space: 14.1 MB (99.33%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -4,6 +4,8 @@ class Solution:
         
         lookup  = Counter(s1)
         count = 0
+
+    
         
         for r in range(len(s2)):
             if s2[r] in lookup:
@@ -11,14 +13,15 @@ class Solution:
                     count += 1
                 lookup[s2[r]] -= 1
             
-            while r-l+1 > len(s1):
+            while (r-l+1) > len(s1):
                 if s2[l] in lookup:
                     lookup[s2[l]] += 1
                     if lookup[s2[l]] > 0:
                         count -= 1
-                l+=1
+                l += 1
             
             if count == len(s1):
                 return True
         return False
+        
         
