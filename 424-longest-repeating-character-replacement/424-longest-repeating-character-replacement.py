c09b403d5ff7acedad623e33af2c8fe8c1a1912c
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-01-27 20:26:09 -0500

    Time: 269 ms (20.93%), Space: 14 MB (98.93%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -5,13 +5,10 @@ class Solution:
         count = defaultdict(int)
         
         for r in range(len(s)):
-            
             count[s[r]]+=1
             
             while (r-l+1) - max(count.values()) > k:
                 count[s[l]] -= 1
-                if count[s[l]]<1:
-                    count.pop(s[l])
                 l+=1
             longestFound = max(longestFound, r-l+1)
         return longestFound
