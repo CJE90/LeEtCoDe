430c14a80797c43c1b2c5a91b426dc459b0d072b
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-01-31 20:37:38 -0500

    Time: 115 ms (34.96%), Space: 15.7 MB (64.61%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -1,37 +1,19 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        # max_left = max_right = tmp = 0
-        # for i in range(len(height)):
-        #     max_left = max(height[i], max_left)
-        #     max_right = max(height[-1-i], max_right)
-        #     tmp += max_left + max_right - height[i]
-        # result = tmp - len(height) * max_left
-        # return result
-
-        leftStack = []
-        maxleft = 0
-        maxright = 0
-        rightStack = []
-        #mins = []
+        l = 0
+        r = len(height)-1
+        maxL = height[l]
+        maxR = height[r]
         tw = 0
         
-        for i in range(len(height)):
-            leftStack.append(maxleft)
-            maxleft = max(maxleft, height[i])
-        for j in range(len(height)-1,-1,-1):
-            rightStack.append(maxright)
-            maxright = max(maxright, height[j])
-        rightStack.reverse()
-        
-#         for i in range(len(height)):
-#             mins.append(min(leftStack[i], rightStack[i]))
-        
-        for i in range(len(height)):
-            a = min(leftStack[i],rightStack[i])
-            if a - height[i] >= 0:
-                tw += a - height[i]
+        while l < r:
+            if maxL < maxR:
+                l+=1
+                maxL = max(maxL,height[l])
+                tw += maxL - height[l]
+            else:
+                r-=1
+                maxR = max(maxR, height[r])
+                tw += maxR - height[r]
         return tw
                 
-            
-        
-        
