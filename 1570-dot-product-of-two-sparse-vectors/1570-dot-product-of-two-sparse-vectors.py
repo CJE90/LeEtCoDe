fb4d5eb3a1f80c395fc612eb766bcef439e0eabe
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-02-02 19:28:23 -0500

    Time: 2415 ms (43.87%), Space: 18.3 MB (75.79%) - LeetHub

--- 1570-dot-product-of-two-sparse-vectors/1570-dot-product-of-two-sparse-vectors.py
+++ 1570-dot-product-of-two-sparse-vectors/1570-dot-product-of-two-sparse-vectors.py
@@ -1,18 +1,13 @@
 class SparseVector:
     def __init__(self, nums: List[int]):
-        self.ht = defaultdict(int)
-        for i in range(len(nums)):
-            if nums[i] != 0:
-                self.ht[i] = nums[i]
-        
+        self.array = nums
         
 
     # Return the dotProduct of two sparse vectors
     def dotProduct(self, vec: 'SparseVector') -> int:
         result = 0
-        for k, v in enumerate(self.ht):
-            if v in vec.ht:
-                result += self.ht[v] * vec.ht[v]
+        for num1, num2 in zip(self.array, vec.array):
+            result += num1*num2
         return result
         
 
@@ -20,4 +15,3 @@ class SparseVector:
 # v1 = SparseVector(nums1)
 # v2 = SparseVector(nums2)
 # ans = v1.dotProduct(v2)
-
