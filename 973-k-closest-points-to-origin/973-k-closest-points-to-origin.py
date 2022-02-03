96ab53064b7e586b696789b541c3b495850615f8
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-02-03 18:44:18 -0500

    Time: 1029 ms (33.93%), Space: 20 MB (45.65%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -1,8 +1,9 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         res = []
-        arr = sorted(points,key=lambda x: math.sqrt(x[0]*x[0] + x[1]*x[1]))
-        for i, p in enumerate(arr):
+        points.sort(key=lambda x: math.sqrt(x[0]*x[0] + x[1]*x[1]))
+        #arr = sorted(points,key=lambda x: math.sqrt(x[0]*x[0] + x[1]*x[1]))
+        for i, p in enumerate(points):
             if i>k-1:
                 return res
             res.append(p)
