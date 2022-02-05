6e0dc66ec4cf890d5212630234bced357fe1fccd
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-02-05 12:15:43 -0500

    Time: 76 ms (70.70%), Space: 18.8 MB (9.92%) - LeetHub

--- 285-inorder-successor-in-bst/285-inorder-successor-in-bst.py
+++ 285-inorder-successor-in-bst/285-inorder-successor-in-bst.py
@@ -7,17 +7,16 @@
 
 class Solution:
     def inorderSuccessor(self, root: TreeNode, p: TreeNode) -> Optional[TreeNode]:
-        self.order = []
-        self.dfs(root)
-        for i in range(len(self.order)-1):
-            if self.order[i].val == p.val:
-                return self.order[i+1]
-        return None
-    
-    def dfs(self, root):
-        if root == None:
-            return 
-        self.dfs(root.left)
-        self.order.append(root)
-        self.dfs(root.right)
-        return
+        result = None
+        
+        while root:
+            if root.val>p.val:
+                result = root
+                root = root.left
+            else:
+                root = root.right
+        return result
+        
+        
+        
+        
