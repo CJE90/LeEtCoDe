ca1ffbcdfe07edffa5db7a80a980afb5f2c282e9
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-02-11 19:09:55 -0500

    Time: 127 ms (68.89%), Space: 14.2 MB (85.09%) - LeetHub

---
+++ 1101-the-earliest-moment-when-everyone-become-friends/1101-the-earliest-moment-when-everyone-become-friends.py
@@ -0,0 +1,42 @@
+class Solution:
+    def earliestAcq(self, logs: List[List[int]], n: int) -> int:
+        a = sorted(logs, key=lambda x: x[0])
+
+        UF = UnionByRank(n)
+        for meeting in a:
+            UF.union(meeting[1], meeting[2])
+            if UF.n == 1:
+                return meeting[0]
+        return -1
+        
+        
+        
+class UnionByRank:
+    def __init__(self, size):
+        self.root = [i for i in range(size)]
+        self.rank = [1 for i in range(size)]
+        self.n = size
+    
+    def find(self, x):
+        while self.root[x] != x:
+            x = self.root[x]
+        return x
+
+    def connected(self, x, y):
+        return self.find(x) == self.find(y)
+
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+
+        if rootX != rootY:
+            self.n -= 1
+            if self.rank[rootX] > self.rank[rootY]:
+                self.root[rootY] = rootX
+            elif self.rank[rootX] < self.rank[rootY]:
+                self.root[rootX] = rootY
+            else:
+                self.root[rootY] = rootX
+                self.rank[rootX] += 1
+        
+        
