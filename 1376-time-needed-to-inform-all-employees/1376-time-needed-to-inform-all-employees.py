d479518bb7e8ad1c72c1666da696df5bc910fa5c
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-02-16 07:43:42 -0500

    Time: 1990 ms (48.48%), Space: 51.5 MB (32.76%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
@@ -1,16 +1,24 @@
 class Solution:
     def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
         graph = defaultdict(list)
+        self.result = 0
         for emp_index, mgr in enumerate(manager):
             if mgr != -1:
                 graph[mgr].append(emp_index)
-        def dfs(headID):
-            ans = 0
-            for employee in graph[headID]:
-                ans = max(ans, dfs(employee)+informTime[headID])
-            return ans
-        
-        return dfs(headID)
+        def dfs(headID, time):
+            # ans = 0
+            # for employee in graph[headID]:
+            #     ans = max(ans, dfs(employee)+informTime[headID])
+            # return ans
+            self.result = max(self.result, time)
+            for emp in graph[headID]:
+                dfs(emp, time+informTime[headID])
+            return self.result
+            
+            
+            
+        result = dfs(headID, 0)
+        return result
         
         
         
