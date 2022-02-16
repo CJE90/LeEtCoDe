ea2fd8ad30e4343b6d3f9391d62ced4a3c63e427
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-02-15 20:22:12 -0500

    Time: 322 ms (79.29%), Space: 16.1 MB (99.85%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,27 +1,54 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
+        result = 0
         rowLen = len(grid)
         colLen = len(grid[0])
-        result = 0
+        que = deque([])
         
-        def dfs(grid, r, c):
-            if r < 0 or r>= rowLen or c < 0 or c >= colLen or grid[r][c] != "1":
-                return
-            grid[r][c] = "0"
-            dfs(grid, r-1, c)
-            dfs(grid, r+1, c)
-            dfs(grid, r, c-1)
-            dfs(grid, r, c+1)
-            return
-        for i in range(rowLen):
-            for j in range(colLen):
-                if grid[i][j] == "1":
-                    dfs(grid, i, j)
+        def bfs(grid, que):
+            while que:
+                I,J = que.popleft()
+                for i,j in [I-1,J],[I+1,J],[I,J-1],[I,J+1]:
+                    if 0 <= i < rowLen and 0 <= j < colLen and grid[i][j] == "1":
+                        que.append((i,j))
+                        grid[i][j] = "0"
+        
+        for r in range(rowLen):
+            for c in range(colLen):
+                if grid[r][c] == "1":
+                    grid[r][c] = "0"
+                    que.append((r,c))
+                    bfs(grid, que)
                     result += 1
         return result
-            
         
         
+        
+        
+        
+        
+        
+        '''
+        class Solution:
+    def numIslands(self, grid):
+        count = 0
+        queue = deque([])
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    grid[i][j] = 0
+                    queue.append((i,j))
+                    self.helper(grid,queue) # turn the adjancent '1' to '0'
+                    count += 1
+        print(grid)
+        return count
+    
+    def helper(self,grid,queue):
+        while queue:
+            I,J = queue.popleft()
+            for i,j in [I-1,J],[I+1,J],[I,J-1],[I,J+1]:
+                if 0<= i < len(grid) and 0 <= j < len(grid[0]) and grid[i][j] == '1':
+                    queue.append((i,j))
+                    grid[i][j] = 0 #0
+        '''
         
