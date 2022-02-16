9c0725aa081b354ce6a44d307f6cb5685ab3db6c
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-02-16 07:27:27 -0500

    Time: 1424 ms (79.10%), Space: 51.7 MB (30.90%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
@@ -1,31 +1,17 @@
 class Solution:
     def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
-        q = deque([(headID, 0)])
-        result = 0
         graph = defaultdict(list)
-        for index, mgr in enumerate(manager):
-            graph[mgr].append(index)
-        while q:
-            curEmp, elapsedTime = q.popleft()
-            result = max(result, elapsedTime)
-            for subordinate in graph[curEmp]:
-                q.append([subordinate, elapsedTime + informTime[curEmp]])
-        return result
-    
-    
-    '''
-    def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
-        q = collections.deque([(headID, 0)])
-        subordinates = collections.defaultdict(list)
-        res = 0
-        for i, v in enumerate(manager):
-            subordinates[v].append(i)
-            
-        while q:
-            u, time = q.popleft()
-            res = max(res, time)
-            for v in subordinates[u]:
-                q.append((v, time + informTime[u]))
-        return res
-    '''
+        for emp_index, mgr in enumerate(manager):
+            if mgr != -1:
+                graph[mgr].append(emp_index)
+        def dfs(headID):
+            ans = 0
+            for employee in graph[headID]:
+                ans = max(ans, dfs(employee)+informTime[headID])
+            return ans
+        
+        return dfs(headID)
+        
+        
+        
         
