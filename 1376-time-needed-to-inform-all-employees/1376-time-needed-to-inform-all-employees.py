da0eec4a496335d30b54fd4585c2d034d94f998e
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-02-15 19:17:06 -0500

    Time: 2132 ms (40.13%), Space: 42.9 MB (70.00%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
@@ -1,15 +1,31 @@
 class Solution:
     def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
-        if n<= 1:
-            return 0
-        lookup = defaultdict(list)
+        q = deque([(headID, 0)])
+        result = 0
+        graph = defaultdict(list)
         for index, mgr in enumerate(manager):
-            lookup[mgr].append(index)
-        
-        def dfs(employee):
-            ans = 0
-            for developer in lookup[employee]:
-                ans = max(ans, dfs(developer)+informTime[employee])
-            return ans
-        return dfs(headID)
+            graph[mgr].append(index)
+        while q:
+            curEmp, elapsedTime = q.popleft()
+            result = max(result, elapsedTime)
+            for subordinate in graph[curEmp]:
+                q.append([subordinate, elapsedTime + informTime[curEmp]])
+        return result
+    
+    
+    '''
+    def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
+        q = collections.deque([(headID, 0)])
+        subordinates = collections.defaultdict(list)
+        res = 0
+        for i, v in enumerate(manager):
+            subordinates[v].append(i)
+            
+        while q:
+            u, time = q.popleft()
+            res = max(res, time)
+            for v in subordinates[u]:
+                q.append((v, time + informTime[u]))
+        return res
+    '''
         
