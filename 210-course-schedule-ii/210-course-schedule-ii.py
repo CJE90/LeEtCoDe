29821ef932e99b6034d825979c71e7c2c3ce487f
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-02-19 15:59:18 -0500

    Time: 181 ms (22.96%), Space: 15.4 MB (87.79%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -1,18 +1,17 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         result = []
-        inDegree = {i:0 for i in range(numCourses)}
         adjList = {i:[] for i in range(numCourses)}
+        inDegree = {i:0 for i in range(numCourses)}
         
-        for course, preReq in prerequisites:
-            adjList[preReq].append(course)
+        for course, prereq in prerequisites:
+            adjList[prereq].append(course)
             inDegree[course] += 1
-        
+            
         que = deque()
         for node in inDegree:
             if inDegree[node] == 0:
                 que.append(node)
-                
         while que:
             node = que.popleft()
             result.append(node)
@@ -23,4 +22,5 @@ class Solution:
         if len(result) == numCourses:
             return result
         return []
+            
         
