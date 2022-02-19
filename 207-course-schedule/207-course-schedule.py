d79eb4ea0d4ac51f78082a79690f92632240c82c
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-02-19 13:56:43 -0500

    Time: 112 ms (66.10%), Space: 17.1 MB (40.06%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -2,31 +2,25 @@ class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         adjList = defaultdict(list)
         visited = set()
-
-        for course, prereq in prerequisites:
-            adjList[course].append(prereq)
-
+        for course, preReq in prerequisites:
+            adjList[course].append(preReq)
+            
         def dfs(node):
             if node in visited:
                 return False
             if not adjList[node]:
                 return True
-            
             visited.add(node)
-            for c in adjList[node]:
-                if not dfs(c): return False
-                
+            for prereq in adjList[node]:
+                if not dfs(prereq):
+                    return False
             visited.remove(node)
-            adjList[node] = []
+            adjList[node] = []    
+            
             return True
-        
         
         for i in range(numCourses):
             if not dfs(i):
                 return False
-            
         return True
-            
-        
-        
         
