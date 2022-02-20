32341183dcd64c1197c4d208d95f357ff9de3f95
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-02-19 19:06:49 -0500

    Time: 52 ms (41.17%), Space: 13.9 MB (98.22%) - LeetHub

---
+++ 269-alien-dictionary/269-alien-dictionary.py
@@ -0,0 +1,36 @@
+class Solution:
+    def alienOrder(self, words: List[str]) -> str:
+        result = []
+        adjList = defaultdict(set)
+        inDegree = {c:0 for w in words for c in w}
+        for i in range(len(words)-1):
+            word1 = words[i]
+            word2 = words[i+1]
+            if len(word1) > len(word2) and word2 == word1[: len(word2)]:
+                    return ""
+            for j in range(len(word1)):
+                if word1[j] == word2[j]:
+                    continue
+                
+                if word2[j] not in adjList[word1[j]]:
+                    inDegree[word2[j]] +=1
+                adjList[word1[j]].add(word2[j])
+                break
+        que = deque()
+        for c in inDegree:
+            if inDegree[c] == 0:
+                que.append(c)
+        while que:
+            c = que.popleft()
+            result.append(c)
+            for child in adjList[c]:
+                inDegree[child] -= 1
+                if inDegree[child] == 0:
+                    que.append(child)
+        res = "".join(result)
+        if len(res) == len(inDegree):
+            return res
+        return ""
+        
+
+        
