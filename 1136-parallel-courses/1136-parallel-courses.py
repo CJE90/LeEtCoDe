426d2359a94ffc2b4bebe799f6af8ce7a47e17f1
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-02-19 19:21:36 -0500

    Time: 422 ms (36.37%), Space: 16.8 MB (85.16%) - LeetHub

--- 1136-parallel-courses/1136-parallel-courses.py
+++ 1136-parallel-courses/1136-parallel-courses.py
@@ -1,6 +1,7 @@
 class Solution:
     def minimumSemesters(self, n: int, relations: List[List[int]]) -> int:
         semesters = 0
+        
         que = deque()
         adjList = {i:[] for i in range(1,n+1)}
         inDegree = {i:0 for i in range(1,n+1)}
@@ -10,18 +11,17 @@ class Solution:
         for node in inDegree:
             if inDegree[node] == 0:
                 que.append(node)
+        studied = len(que)
         while que:
             n = len(que)
             for _ in range(n):
+                
                 node = que.popleft()
                 for child in adjList[node]:
+                    #studied +=1
                     inDegree[child] -= 1
                     if inDegree[child] == 0:
+                        studied +=1
                         que.append(child)
             semesters += 1
-            
-            
-        for node in inDegree:
-            if inDegree[node] != 0:
-                return -1
-        return semesters
+        return semesters if studied == len(inDegree) else -1
