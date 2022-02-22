98f77a7bb0bf96e5277e0adad5dab0316d04e3cb
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-02-22 18:42:55 -0500

    Time: 140 ms (52.59%), Space: 15.5 MB (87.12%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,26 +1,29 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        if numCourses ==0:
+            return False
         adjList = defaultdict(list)
-        visited = set()
-        for course, preReq in prerequisites:
-            adjList[course].append(preReq)
-            
-        def dfs(node):
-            if node in visited:
-                return False
-            if not adjList[node]:
-                return True
-            visited.add(node)
-            for prereq in adjList[node]:
-                if not dfs(prereq):
-                    return False
-            visited.remove(node)
-            adjList[node] = []    
-            
-            return True
+        indegree = {i:0 for i in range(numCourses)}
         
-        for i in range(numCourses):
-            if not dfs(i):
+        for course, prereq in prerequisites:
+            adjList[prereq].append(course)
+            indegree[course] += 1
+        
+        que = deque()
+        for course in indegree:
+            if indegree[course] == 0:
+                que.append(course)
+        
+        while que:
+            node = que.popleft()
+            for child in adjList[node]:
+                indegree[child] -= 1
+                if indegree[child] == 0:
+                    que.append(child)
+        
+        for val in indegree:
+            if indegree[val] != 0:
                 return False
         return True
+                
         
