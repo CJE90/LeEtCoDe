2dac6a4b83c991c7cf3308351042d4252d2b2463
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-02-22 22:34:01 -0500

    Time: 5980 ms (80.02%), Space: 14 MB (59.38%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -3,7 +3,6 @@ class Solution:
         ROWS, COLS = len(board), len(board[0])
         path = set()
         
-        
         def dfs(r,c,i):
             if i == len(word):
                 return True
@@ -16,15 +15,12 @@ class Solution:
                    dfs(r,c-1,i+1))
             path.remove((r,c))
             return res
-            
-        
-        
-        
-        
+                   
         for i in range(ROWS):
             for j in range(COLS):
-                if dfs(i,j,0):
-                    return True
+                if board[i][j] == word[0]:
+                    if dfs(i,j,0):
+                        return True
         return False
                 
         
