8c556dc88cb21b4bec4686c1c94f31f9f8f45f40
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-02-22 19:18:18 -0500

    Time: 419 ms (54.10%), Space: 25.6 MB (6.23%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -2,14 +2,14 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         rowLen = len(grid)
         colLen = len(grid[0])
-        bools = [[False for i in range(colLen)] for j in range(rowLen)]
+        hashSet = set()
         
         count = 0
         
         def sink(grid, i, j) -> None:
-            if i < 0 or i >= rowLen or j < 0 or j >= colLen or grid[i][j] != "1" or bools[i][j] == True:
+            if i < 0 or i >= rowLen or j < 0 or j >= colLen or grid[i][j] != "1" or (i,j) in hashSet:
                 return
-            bools[i][j] =True
+            hashSet.add((i,j))
             
             sink(grid, i-1, j)
             sink(grid, i+1, j)
@@ -20,7 +20,7 @@ class Solution:
         
         for i in range(rowLen):
             for j in range(colLen):
-                if grid[i][j] == "1" and bools[i][j] != True:
+                if grid[i][j] == "1" and (i,j) not in hashSet:
                     sink(grid, i, j)
                     count += 1
                     
