327e3f837f6bd0e83d4700307c57c4bdcb86558d
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-02-23 07:17:56 -0500

    Time: 84 ms (13.94%), Space: 13.9 MB (78.11%) - LeetHub

---
+++ 299-bulls-and-cows/299-bulls-and-cows.py
@@ -0,0 +1,18 @@
+class Solution:
+    def getHint(self, secret: str, guess: str) -> str:
+        lookup = Counter(secret)
+        bull = 0
+        cow = 0
+        
+        for index,char in enumerate(guess):
+            if secret[index]==guess[index]:
+                bull += 1
+                lookup[char] -= 1    
+        for index,char in enumerate(guess):
+            if secret[index]!=guess[index] and char in lookup:
+                if lookup[char] >0:
+                    cow += 1
+                    lookup[char] -= 1
+        return str(bull)+'A'+str(cow)+'B'
+            
+        
