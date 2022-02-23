c895ed273cada888bec15c9ec352356d509975cd
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-02-23 11:33:46 -0500

    Time: 49 ms (30.74%), Space: 14 MB (63.27%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,18 +1,15 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         hashTable = {}
+        
         def dfs(n):
             if n <= 2:
-                hashTable[n] = n
-                return hashTable[n]
+                return n
             if n in hashTable:
                 return hashTable[n]
-            
-            
+                        
             hashTable[n] = dfs(n-1)+dfs(n-2)
             return hashTable[n]
-            
-            
-        
+                
         return dfs(n)
         
