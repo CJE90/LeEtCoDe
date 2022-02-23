3c20e2fc020116e74f72ca61840b81e13d41989e
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-02-23 17:45:00 -0500

    Time: 116 ms (11.86%), Space: 16.2 MB (16.42%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -3,21 +3,21 @@ class Solution:
         if len(cost) == 1:
             return cost[0]
         
-        dp = [0] * (len(cost)+1)
-        dp[0] = 0
-        dp[1] = 0
-        for i in range(2,len(cost)+1):
-            dp[i] = min(cost[i-1]+dp[i-1],cost[i-2]+dp[i-2])
-        return dp[len(cost)]
-        # memo = {}
-        # def step(n):
-        #     if n == 0:
-        #         return 0
-        #     if n == 1:
-        #         return 0
-        #     if n not in memo:
-        #         memo[n] = cost[n]+min(cost[n]+step(n-2), step(n-1))
-        #     return memo[n]
-        # return step(len(cost)-1)
+        # dp = [0] * (len(cost)+1)
+        # dp[0] = 0
+        # dp[1] = 0
+        # for i in range(2,len(cost)+1):
+        #     dp[i] = min(cost[i-1]+dp[i-1],cost[i-2]+dp[i-2])
+        # return dp[len(cost)]
+        memo = {}
+        def dp(n):
+            if n == 0:
+                return 0
+            if n == 1:
+                return 0
+            if n not in memo:
+                memo[n] = min(cost[n-1]+dp(n-1), cost[n-2]+dp(n-2))
+            return memo[n]
+        return dp(len(cost))
         
         
