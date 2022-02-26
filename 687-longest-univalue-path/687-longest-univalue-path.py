c11228612e494f2f6310404754bae16752e4bcb3
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-02-25 19:05:22 -0500

    Time: 657 ms (19.70%), Space: 17.8 MB (95.99%) - LeetHub

--- 687-longest-univalue-path/687-longest-univalue-path.py
+++ 687-longest-univalue-path/687-longest-univalue-path.py
@@ -6,16 +6,13 @@
 #         self.right = right
 class Solution:
     def longestUnivaluePath(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
-        
-        maxPath = 0
+        maxEdges = 0
         
         def dfs(node):
-            nonlocal maxPath
-            
+            nonlocal maxEdges
             if not node:
                 return 0
+            
             left = dfs(node.left)
             right = dfs(node.right)
             leftPath = 0
@@ -23,12 +20,12 @@ class Solution:
             if node.left and node.left.val == node.val:
                 leftPath  = left + 1
             if node.right and node.right.val == node.val:
-                rightPath  = right + 1
-                
-            maxPath = max(maxPath, leftPath+rightPath)
-            return max(leftPath, rightPath)
+                rightPath = right+1
+            maxEdges = max(maxEdges, leftPath+rightPath)
+            return max(leftPath,rightPath)
+            
+            
             
         dfs(root)
-        return maxPath
-            
+        return maxEdges
         
