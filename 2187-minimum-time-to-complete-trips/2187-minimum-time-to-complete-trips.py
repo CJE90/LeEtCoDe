e4a72551efe4c72becb7be85816a530e6e3cedc4
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-02-27 00:12:17 -0500

    Time: 3592 ms (70.00%), Space: 28.5 MB (80.00%) - LeetHub

---
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minimumTime(self, time: List[int], totalTrips: int) -> int:
+        if len(time) == 1:
+            return time[0]*totalTrips
+        lo = 1
+        hi = max(time)*totalTrips
+        
+        while lo < hi:
+            mid = lo + (hi - lo) //2
+            
+            def trip(seconds):
+                curSum = 0
+                for v in time:
+                    curSum += seconds//v
+                return curSum >= totalTrips
+            
+            
+            if trip(mid):
+                hi = mid
+            else:
+                lo = mid+1
+        return hi
