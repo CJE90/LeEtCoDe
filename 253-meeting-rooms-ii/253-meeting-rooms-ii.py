c19325122bdddd798fd814fd5a38673768695dce
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-02-26 20:15:46 -0500

    Time: 86 ms (74.74%), Space: 17.5 MB (88.37%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -1,15 +1,12 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        value_list = []
-        temp_value = 0
-        res = 0
-        
-        for interval in intervals:
-            value_list.append((interval[0], 1))
-            value_list.append((interval[1], -1))
-        
-        for value in sorted(value_list):
-            temp_value += value[1]
-            res = max(res, temp_value)
-            
-        return res
+        intervals.sort(key=lambda x:x[0])
+        heap = []  # stores the end time of intervals
+        for i in intervals:
+            if heap and i[0] >= heap[0]: 
+            # means two intervals can use the same room
+                heapq.heapreplace(heap, i[1])
+            else:
+            # a new room is allocated
+                heapq.heappush(heap, i[1])
+        return len(heap)
