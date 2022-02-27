63bcba747d102de06d9daab6d46cff06c3e04ade
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-02-27 15:44:18 -0500

    Time: 74 ms (76.95%), Space: 14.8 MB (72.06%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -5,7 +5,8 @@ class Solution:
             min_heap.append(nums[i])
         heapq.heapify(min_heap)
         for i in range(k, len(nums)):
-            heapq.heappush(min_heap, nums[i])
+            if nums[i]>= min_heap[0]:
+                heapq.heappush(min_heap, nums[i])
             if len(min_heap)>k:
                 heapq.heappop(min_heap)
         return min_heap[0]
