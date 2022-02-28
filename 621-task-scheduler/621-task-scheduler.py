b7b752246cd31d82f26183a06a5e4e2a59a4b2ef
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-02-27 19:42:01 -0500

    Time: 1346 ms (6.53%), Space: 14.5 MB (51.63%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.py
+++ 621-task-scheduler/621-task-scheduler.py
@@ -19,6 +19,16 @@ class Solution:
                     heapq.heappush(maxheap, que.popleft()[0])
         
         return time
+    '''
+    1. create hashmap for character frequency
+    2. convert that character frequency count to maxHeap
+        i. We want to always process the most frequent task
+    3. Pop the most frequent task and reduce its count
+        i. add it to the que with the time it can be released
+    4. increment Time by one becuase we are processing a task
+    5. If the task at the head of the que can be processed again. pop it and add it back to the heap
+    6. once all task are processed return the time
+    '''
             
         
         
