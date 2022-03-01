54fd675a74422d242e1562a883c69a782eab203d
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-02-28 19:24:16 -0500

    Time: 187 ms (34.45%), Space: 17.7 MB (71.53%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -8,6 +8,7 @@ class Solution:
         ListNode.__lt__ = lambda self, other: self.val < other.val
         min_heap = []
         head = tail = ListNode(0)
+        
         for i in lists:
             if i:
                 heapq.heappush(min_heap,(i.val, i))
@@ -18,25 +19,4 @@ class Solution:
             if node.next:
                 heapq.heappush(min_heap,(node.next.val, node.next))
         return head.next
-        
-        
-        
-        '''
-    ListNode.__eq__ = lambda self, other: self.val == other.val
-	ListNode.__lt__ = lambda self, other: self.val < other.val
-	h = []
-	head = tail = ListNode(0)
-	for i in lists:
-		if i:
-			heapq.heappush(h, (i.val, i))
-
-	while h:
-		node = heapq.heappop(h)[1]
-		tail.next = node
-		tail = tail.next
-		if node.next:
-			heapq.heappush(h, (node.next.val, node.next))
-
-	return head.next
-        '''
         
