230951a2e5d8a4caa190b2ef4d967e62bcbd87d9
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-03-03 18:27:18 -0500

    Time: 848 ms (81.52%), Space: 20.5 MB (12.31%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.py
+++ 904-fruit-into-baskets/904-fruit-into-baskets.py
@@ -1,18 +1,16 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
+        pickedFruits = defaultdict(int)
         l = 0
-        maxFruit = 0
-        holding = defaultdict(int)
-        
+        maxLength = 0
         for r in range(len(fruits)):
-            holding[fruits[r]] += 1
-            
-            while len(holding) > 2:
-                holding[fruits[l]] -= 1
-                if holding[fruits[l]] < 1:
-                    holding.pop(fruits[l])
-                l+=1
-            
-            maxFruit = max(maxFruit, r-l+1)
-        return maxFruit
+            pickedFruits[fruits[r]] += 1
+            while len(pickedFruits) > 2:
+                pickedFruits[fruits[l]] -= 1
+                if pickedFruits[fruits[l]] < 1:
+                    pickedFruits.pop(fruits[l])
+                l += 1
+            maxLength = max(maxLength, r-l+1)
+        return maxLength
+                
         
