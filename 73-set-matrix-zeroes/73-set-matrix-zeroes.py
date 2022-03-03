a9f60df0b61dc6a96fc7bb25bb2e71fa7e48b90a
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-03-02 19:19:27 -0500

    Time: 136 ms (82.12%), Space: 14.8 MB (55.15%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -3,19 +3,26 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        que = deque()
+        colMarked = False
         for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
+            if matrix[i][0] == 0:
+                colMarked = True
+            for j in range(1,len(matrix[0])):
+                
                 if matrix[i][j] == 0:
-                    que.append([i,j])
-        while que:
-            x,y = que.popleft()
-            for i in range(len(matrix[0])):
-                matrix[x][i] = 0
-            for j in range(len(matrix)):
-                matrix[j][y] = 0
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+                    
+        for i in range(1,len(matrix)):
+            for j in range(1, len(matrix[0])):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+        if matrix[0][0] == 0:
+            for j in range(len(matrix[0])):
+                matrix[0][j] = 0
+        if colMarked:
+            for i in range(len(matrix)):
+                matrix[i][0] = 0
+                    
         
         
-                              
-            
-            
