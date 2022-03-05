f89a523745758f4f7e5f718d48e6868f1b59614c
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-03-04 19:53:42 -0500

    Time: 1002 ms (75.53%), Space: 59.6 MB (92.08%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.py
+++ 41-first-missing-positive/41-first-missing-positive.py
@@ -1,8 +1,14 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        seen = set(nums)
-        for i in range(len(nums)):
-            if i+1 not in seen:
-                return i+1
-        return len(nums)+1
+        nums.append(0)
+        n = len(nums)
+        for i in range(len(nums)): #delete those useless elements
+            if nums[i]<0 or nums[i]>=n:
+                nums[i]=0
+        for i in range(len(nums)): #use the index as the hash to record the frequency of each number
+            nums[nums[i]%n]+=n
+        for i in range(1,len(nums)):
+            if nums[i]//n==0:
+                return i
+        return n
         
