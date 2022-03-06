99dd6a2118f82e228aba07f966abd1c0d9b0d848
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-03-05 19:43:10 -0500

    Time: 876 ms (35.55%), Space: 37.5 MB (5.25%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -16,21 +16,16 @@ class Solution:
             exploreAndCount(r,c-1,countModifier)
             return
         
-        
-        r = 0
-        while r<len(grid):
+        for r in range(len(grid)):
             if grid[r][0] == 1:
                 exploreAndCount(r,0,0)
             if grid[r][len(grid[0])-1] == 1:
                 exploreAndCount(r,len(grid[0])-1, 0)
-            r+=1
-        c = 0
-        while c<len(grid[0]):
+        for c in range(len(grid[0])):
             if grid[0][c] == 1:
                 exploreAndCount(0,c,0)
             if grid[len(grid)-1][c] == 1:
                 exploreAndCount(len(grid)-1, c, 0)
-            c+=1
         
         for r in range(1,len(grid)-1):
             for c in range(1,len(grid[0])-1):
