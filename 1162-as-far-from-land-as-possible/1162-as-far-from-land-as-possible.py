2db5b31b156e55129097d8d13ab5728708481f2b
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-03-05 20:41:14 -0500

    Time: 805 ms (54.65%), Space: 14.5 MB (89.57%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.py
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.py
@@ -1,28 +1,24 @@
 class Solution:
     def maxDistance(self, grid: List[List[int]]) -> int:
-        #maxDistance = 0
-        rowLen = len(grid)
-        colLen = len(grid[0])
-        count = 0
+        m,n = len(grid), len(grid[0])
         que = deque()
+        steps = 0
         
-        for r in range(rowLen):
-            for c in range(colLen):
-                if grid[r][c] == 1:
-                    que.append((r,c))
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    que.append((i,j))
         
         while que:
-            
             size = len(que)
             
             for _ in range(size):
                 r,c = que.popleft()
-                for direction in ((1,0),(-1,0),(0,1),(0,-1)):
-                    newR, newC = r+direction[0], c+direction[1]
-                    if newR >= 0 and newR < rowLen and newC >= 0 and newC < colLen and grid[newR][newC] == 0:
-                        grid[newR][newC] = 1
-                        #maxDistance = max(maxDistance, grid[newR][newC])
-                        que.append((newR, newC))
-            count+=1
-        return count - 1 if count - 1 != 0 else -1
+                for newRow, newCol in [[r+1,c],[r-1,c],[r,c+1],[r,c-1]]:
+                    if 0<=newRow<m and 0<=newCol<n and grid[newRow][newCol] == 0:
+                        grid[newRow][newCol] = 1
+                        que.append((newRow,newCol))
+            steps += 1
+        return steps-1 if steps - 1 != 0 else -1
+        
         
