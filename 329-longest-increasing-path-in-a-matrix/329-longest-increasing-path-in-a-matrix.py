5087114559a6ab4e4c55d1383bd993007de9f95d
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-03-06 12:40:21 -0500

    Time: 524 ms (77.46%), Space: 19.7 MB (13.79%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
@@ -1,25 +1,35 @@
+from collections import deque
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
-        maxLen = 0
-        graph = collections.defaultdict(list)
-        indegree = collections.defaultdict(int)
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                neighbors = [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]
-                for x, y in neighbors:
-                    if 0<=x<len(matrix) and 0<=y<len(matrix[0]) and matrix[i][j] < matrix[x][y]:
-                        graph[(i,j)].append((x,y))
-                        indegree[(x,y)]+=1
         
-        queue = collections.deque([(i, j) for i in range(len(matrix)) for j in range(len(matrix[0])) if (i,j) not in indegree])
-        while queue:
-            maxLen +=1
-            n = len(queue)
-            for _ in range(n):
-                node = queue.popleft()
-                for neighbor in graph[node]:
-                    indegree[neighbor]-=1
-                    if indegree[neighbor] == 0:
-                        queue.append(neighbor)
-        return maxLen
+        m = len(matrix)
+        n = len(matrix[0])
+        if m == 1 and n == 1:
+            return 1
+        inDegree = defaultdict(int)
+        adjList = defaultdict(list)
+        steps = 0
+        for i in range(m):
+            for j in range(n):
+                for x,y in [[i+1,j],[i-1,j],[i,j-1],[i,j+1]]:
+                    if 0<=x<m and 0<=y<n and matrix[i][j]<matrix[x][y]:
+                        adjList[(i,j)].append((x,y))
+                        inDegree[(x,y)] += 1
         
+        que = deque()
+        for node in adjList:
+            if node not in inDegree:
+                que.append(node)
+    
+        while que:
+            size = len(que)
+            steps +=1
+            for _ in range(size):
+                node = que.popleft()
+                for child in adjList[node]:
+                    inDegree[child] -= 1
+                    if inDegree[child] == 0:
+                        que.append(child)
+            
+        return steps
+                
