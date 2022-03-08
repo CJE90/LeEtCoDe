1505a86aa5609dc6fc12a0aada167df1345c3e27
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-03-08 18:57:46 -0500

    Time: 7415 ms (48.66%), Space: 13.8 MB (95.37%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -28,3 +28,29 @@ class Solution:
                     if dfs(r,c,0):
                         return True
         return False
+#         visited = set()
+        
+#         def backtrack(i,j, index):
+#             if index >= len(word):
+#                 return True
+            
+#             if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or (i,j) in visited or word[index] != board[i][j]:
+#                 return False
+#             visited.add((i,j))
+#             if (backtrack(i+1,j, index+1) or
+#                 backtrack(i-1,j, index+1) or
+#                 backtrack(i, j+1, index+1) or
+#                 backtrack(i,j-1, index+1)):
+#                 return True
+#             visited.remove((i,j))
+#             return False
+            
+        
+#         for i in range(len(board)):
+#             for j in range(len(board[0])):
+#                 if board[i][j] == word[0]:
+#                     if backtrack(i,j, 0):
+#                         return True
+#         return False
+
+        
