5180be63f4b692003e8290c4ee748f2e35ae5173
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-03-08 19:00:36 -0500

    Time: 67 ms (5.26%), Space: 13.9 MB (57.43%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -5,17 +5,14 @@ class Solution:
         letters = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", 
                    "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
         result = []
-        def backtrack(index,path):
+       
+        def backtrack(index, path):
             if len(path) == len(digits):
                 result.append("".join(path))
                 return
-            possibleLetters = letters[digits[index]]
-            for letter in possibleLetters:
-                path.append(letter)
+            for c in letters[digits[index]]:
+                path.append(c)
                 backtrack(index+1, path)
                 path.pop()
-            
-        
-        result = []
-        backtrack(0, [])
+        backtrack(0,[])
         return result
