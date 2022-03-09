4618c6e373f348d8b2bb122fb70a7ebe4b8e2a59
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-03-09 16:57:23 -0500

    Time: 44 ms (61.61%), Space: 14.1 MB (84.48%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,11 +1,17 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-  
-        ret = []
-        self.dfs(nums, [], ret)
-        return ret
+        
+        result = []
+        self.backtrack(nums, result, [])
+        return result
     
-    def dfs(self, nums, path, ret):
-        ret.append(path)
+    def backtrack(self, nums: List[int], result: List[int], path: List[int]):
+        
+        result.append(path.copy())
         for i in range(len(nums)):
-            self.dfs(nums[i+1:], path+[nums[i]], ret)        
+            path.append(nums[i])
+            self.backtrack(nums[i+1:], result, path)
+            path.pop()
+        
+        
+        
