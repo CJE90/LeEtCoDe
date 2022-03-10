08f70d4558e1272620717664f48c0d71e6bcafda
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-03-10 18:08:07 -0500

    Time: 118 ms (49.84%), Space: 17.2 MB (95.16%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -1,19 +1,77 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        result = []
+        intervals.append(newInterval)
+        intervals.sort(key = lambda x:x[0])
         
-        for i in intervals:
-            if i[1] < newInterval[0]:
-                result.append(i)
-            elif newInterval[1] < i[0]:
-                result.append(newInterval)
-                newInterval = i
+        return self.mergeIntervals(intervals)
+        
+    def mergeIntervals(self, intervals: List[List[int]]) -> List[List[int]]:
+        result = []
+        for interval in intervals:
+            if result and result[-1][1] >= interval[0]:
+                result[-1][1] = max(result[-1][1], interval[1])
             else:
-                newInterval[0] = min(i[0], newInterval[0])
-                newInterval[1] = max(i[1], newInterval[1])
-                
-                
-                
-        result.append(newInterval)
+                result.append(interval)
         return result
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        #         result = []
+        
+#         for i in intervals:
+#             if i[1] < newInterval[0]:
+#                 result.append(i)
+#             elif newInterval[1] < i[0]:
+#                 result.append(newInterval)
+#                 newInterval = i
+#             else:
+#                 newInterval[0] = min(i[0], newInterval[0])
+#                 newInterval[1] = max(i[1], newInterval[1])
+                
+#         result.append(newInterval)
+#         return result
         
