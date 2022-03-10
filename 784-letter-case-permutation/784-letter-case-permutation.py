a2108eddfd6ce159463c8cff88d18effac231ce8
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-03-09 20:51:23 -0500

    Time: 91 ms (42.81%), Space: 15.6 MB (8.11%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.py
+++ 784-letter-case-permutation/784-letter-case-permutation.py
@@ -1,27 +1,42 @@
 class Solution:
     def letterCasePermutation(self, s: str) -> List[str]:
         result = []
-        s = s.lower()
+        S = list(s)
+        
+        def backtrack(index):
+            result.append(''.join(S))
+            for index in range(index, len(S)):
+                if S[index].isalpha():
+                    S[index] = S[index].swapcase()
+                    backtrack(index+1)
+                    S[index] = S[index].swapcase()
+        backtrack(0)
+        return result
         
         
-        def dfs(index, path):
-            if len(path) == len(s):
-                result.append("".join(path.copy()))
-                return
-            if s[index].isdigit():
-                path.append(s[index])
-                dfs(index+1, path)
-            else:
-                path.append(s[index])
-                dfs(index+1, path)
-                path.pop()
-                path.append(s[index].upper())
-                dfs(index+1, path)
-            path.pop()
+        
+#         result = []
+#         s = s.lower()
+        
+        
+#         def dfs(index, path):
+#             if len(path) == len(s):
+#                 result.append("".join(path.copy()))
+#                 return
+#             if s[index].isdigit():
+#                 path.append(s[index])
+#                 dfs(index+1, path)
+#             else:
+#                 path.append(s[index])
+#                 dfs(index+1, path)
+#                 path.pop()
+#                 path.append(s[index].upper())
+#                 dfs(index+1, path)
+#             path.pop()
             
                 
             
-        dfs(0, [])
-        return result
+#         dfs(0, [])
+#         return result
         
         
