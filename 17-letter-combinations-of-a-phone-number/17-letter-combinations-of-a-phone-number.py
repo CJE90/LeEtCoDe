766a1f485241001646bb4c3547cd2f34d793eab9
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-03-09 22:20:18 -0500

    Time: 58 ms (16.37%), Space: 14 MB (35.36%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -1,18 +1,20 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if not digits:
+        if not digits or len(digits) == 0:
             return []
-        letters = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", 
-                   "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
+        lookup = {'2':'abc','3':'def', '4':'ghi','5':'jkl','6':'mno','7':'pqrs','8':'tuv','9':'wxyz'}
+        
         result = []
-       
+        
         def backtrack(index, path):
             if len(path) == len(digits):
                 result.append("".join(path))
                 return
-            for c in letters[digits[index]]:
+            for c in lookup[digits[index]]:
                 path.append(c)
                 backtrack(index+1, path)
                 path.pop()
-        backtrack(0,[])
+            
+        backtrack(0, [])
         return result
+        
