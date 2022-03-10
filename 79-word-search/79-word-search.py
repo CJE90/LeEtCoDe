829a7b3f9fa98722e90bd5c63b92936722a9f598
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-03-09 19:13:46 -0500

    Time: 6200 ms (75.04%), Space: 14 MB (39.91%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,29 +1,28 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-
+        m = len(board)
+        n = len(board[0])
         visited = set()
         
-        def backtrack(i,j, index):
+        def explore(r, c, index):
             if index >= len(word):
                 return True
-            
-            if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or (i,j) in visited or word[index] != board[i][j]:
+            if r < 0 or r >= m or c < 0 or c >= n or (r,c) in visited or board[r][c] != word[index]:
                 return False
-            visited.add((i,j))
-            if (backtrack(i+1,j, index+1) or
-                backtrack(i-1,j, index+1) or
-                backtrack(i, j+1, index+1) or
-                backtrack(i,j-1, index+1)):
-                return True
-            visited.remove((i,j))
-            return False
+            visited.add((r,c))
             
+            if (explore(r+1, c, index+1) or
+                explore(r-1, c, index+1) or
+                explore(r, c+1, index+1) or
+                explore(r, c-1, index+1)):
+                return True
+            visited.remove((r,c))
+            return False
         
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if board[i][j] == word[0]:
-                    if backtrack(i,j, 0):
+        for r in range(m):
+            for c in range(n):
+                if board[r][c] == word[0]:
+                    if explore(r,c,0):
                         return True
         return False
-
         
