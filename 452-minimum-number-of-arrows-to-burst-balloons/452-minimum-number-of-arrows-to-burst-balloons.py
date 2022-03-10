12a40b7bc4867b9b1d48a122e748d8dd90b2c6db
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-03-10 18:55:18 -0500

    Time: 2632 ms (9.92%), Space: 59.9 MB (17.24%) - LeetHub

--- 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.py
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.py
@@ -1,14 +1,73 @@
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
-        needed_arrrows = len(points)
-        points.sort(key = lambda x:x[1])
-        prev_ballon_end = -inf
-        for s,e in points:
-            if s > prev_ballon_end:
-                prev_ballon_end = e
-            else:
-                needed_arrrows -= 1
-        return needed_arrrows
+            arrows = len(points)
+            points.sort(key = lambda x:x[0])
+            prevEnd = -inf
+            for balloon in points:
+                if balloon[0] > prevEnd:
+                    prevEnd = balloon[1]
+                else:
+                    arrows-=1
+                    prevEnd = min(prevEnd, balloon[1])
+            return arrows
+                    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         needed_arrrows = len(points)
+#         points.sort(key = lambda x:x[1])
+#         prev_ballon_end = -inf
+#         for s,e in points:
+#             if s > prev_ballon_end:
+#                 prev_ballon_end = e
+#             else:
+#                 needed_arrrows -= 1
+#         return needed_arrrows
     
     
     
