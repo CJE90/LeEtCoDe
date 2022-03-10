c662c44f3f57ba304ded78f71ce0c23d51d77b12
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-03-09 21:29:04 -0500

    Time: 72 ms (22.20%), Space: 14.2 MB (48.96%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -5,13 +5,18 @@ class Solution:
             if start == end:
                 ans.append(nums[:])
             for i in range(start, end):
+                
                 nums[start], nums[i] = nums[i], nums[start]
+                
                 backtrack(start+1, end)
+                
                 nums[start], nums[i] = nums[i], nums[start]
+                
                 
         ans = []
         backtrack(0, len(nums))
         return ans
+    
         
         
        
