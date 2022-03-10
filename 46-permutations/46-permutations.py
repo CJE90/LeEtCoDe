a1949b808ddbe0df8197f08bc9b2244c2920a83f
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-03-09 21:40:16 -0500

    Time: 63 ms (39.48%), Space: 14.1 MB (48.96%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,22 +1,37 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         result = []
-        if len(nums) == 1:
-            return [nums[:]]
+        n = len(nums)
+        def backtrack(perm):
+            if len(perm) == len(nums):
+                result.append(perm.copy())
+                return
+            for i in range(n):
+                if nums[i] in perm:
+                    continue
+                perm.append(nums[i])
+                backtrack(perm)
+                perm.pop()
+        backtrack([])
+        return result        
         
-        for i in range(len(nums)):
-            val = nums.pop(0)
-            perms = self.permute(nums)
+        
+#--------------------------------------------------------------------------- 
+#         result = []
+#         if len(nums) == 1:
+#             return [nums[:]]
+        
+#         for i in range(len(nums)):
+#             val = nums.pop(0)
+#             perms = self.permute(nums)
             
-            for perm in perms:
-                perm.append(val)
-            result.extend(perms)
+#             for perm in perms:
+#                 perm.append(val)
+#             result.extend(perms)
 
-            nums.append(val)
-        return result
-            
-        
-        
+#             nums.append(val)
+#         return result
+                  
 #---------------------------------------------------------------------------    
 #         def backtrack(start, end):
 #             if start == end:
