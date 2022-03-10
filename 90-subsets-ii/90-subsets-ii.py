64fbc181944fddcaefabc770b135c6e35a0d9da2
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-03-10 06:22:38 -0500

    Time: 40 ms (85.96%), Space: 14.2 MB (37.55%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.py
+++ 90-subsets-ii/90-subsets-ii.py
@@ -1,24 +1,46 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        
         result = []
         subset = []
+        
+        def backtrack(index):
+            if index >= len(nums):
+                result.append(subset.copy())
+                return
+            
+            subset.append(nums[index])
+            backtrack(index+1)
+            subset.pop()
+        
+            while index+1 < len(nums) and nums[index] == nums[index+1]:
+                index += 1
+            
+            
+            backtrack(index+1)
+            
+        backtrack(0)
+        return result        
+        
+#         nums.sort()
+        
+#         result = []
+#         subset = []
        
         
-        def dfs(i):
-            if i >= len(nums):
-                
-                result.append(subset.copy())
-                    
-                return
-            subset.append(nums[i])
-            dfs(i+1)
-            subset.pop()
-            while i + 1 < len(nums) and nums[i] == nums[i+1]:
-                i+=1
-            dfs(i+1)
+#         def dfs(i):
+#             if i >= len(nums):
+#                 result.append(subset.copy())  
+#                 return
             
-        dfs(0)
-        return result
+#             subset.append(nums[i])
+#             dfs(i+1)
+#             subset.pop()
+            
+#             while i + 1 < len(nums) and nums[i] == nums[i+1]:
+#                 i+=1
+#             dfs(i+1)
+            
+#         dfs(0)
+#         return result
         
