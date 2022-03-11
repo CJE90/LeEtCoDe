e57df97fab5db1e9f0c2a48af8d871692c2925d1
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-03-10 22:02:58 -0500

    Time: 100 ms (94.53%), Space: 18.7 MB (46.97%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,15 +1,75 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         lookup = Counter(nums)
-        minHeap = []
+        min_heap = [[-lookup[key], key] for key in lookup.keys()]
+        heapq.heapify(min_heap)
         result = []
-        for element, frequency in lookup.items():
-            heapq.heappush(minHeap, (frequency, element))
-            if len(minHeap) > k:
-                heapq.heappop(minHeap)
-        while minHeap or len(result) < k:
-            _, element = heapq.heappop(minHeap)
-            result.append(element)
+        while k > 0:
+            frequency, character = heapq.heappop(min_heap)
+            result.append(character)
+            k -= 1
         return result
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#         lookup = Counter(nums)
+#         minHeap = []
+#         result = []
+#         for element, frequency in lookup.items():
+#             heapq.heappush(minHeap, (frequency, element))
+#             if len(minHeap) > k:
+#                 heapq.heappop(minHeap)
+#         while minHeap or len(result) < k:
+#             _, element = heapq.heappop(minHeap)
+#             result.append(element)
+#         return result
         
         
