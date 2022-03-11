df0fce7eb44d59bc9d8b30b2cec5fc78e646a040
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-03-10 19:48:32 -0500

    Time: 80 ms (88.95%), Space: 17.4 MB (87.13%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -1,13 +1,76 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        intervals.sort(key= lambda x:x[0])
-        minHeap = []
-        for i in intervals:
-            if minHeap and i[0]>=minHeap[0]:
-                heapq.heapreplace(minHeap, i[1])
+        intervals.sort(key = lambda x : x[0])
+        numOfRooms = []
+        for interval in intervals:
+            if numOfRooms and interval[0] >= numOfRooms[0]:
+                heapq.heappop(numOfRooms)
+                heapq.heappush(numOfRooms, interval[1])
+                
             else:
-                heapq.heappush(minHeap, i[1])
-        return len(minHeap)
+                heapq.heappush(numOfRooms, interval[1])
+                
+        return len(numOfRooms)        
+                
+                
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+#         intervals.sort(key= lambda x:x[0])
+#         minHeap = []
+#         for i in intervals:
+#             if minHeap and i[0]>=minHeap[0]:
+#                 heapq.heapreplace(minHeap, i[1])
+#             else:
+#                 heapq.heappush(minHeap, i[1])
+#         return len(minHeap)
         
         
         
