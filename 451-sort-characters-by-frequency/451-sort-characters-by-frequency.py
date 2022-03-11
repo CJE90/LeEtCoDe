aa1885b5f638bbc2b15ce875596df50cb1094ca4
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-03-11 18:54:11 -0500

    Time: 75 ms (43.37%), Space: 16.3 MB (25.72%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.py
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.py
@@ -1,16 +1,77 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
-        hm = Counter(s)
-        maxHeap = []
-        res = []
-        for key, value in hm.items():
-            heapq.heappush(maxHeap, [-value, key])
-        while maxHeap:
-            c = heapq.heappop(maxHeap)
-            while c[0] < 0:
-                res.append(c[1])
-                c[0] += 1
-        return "".join(res)
+        result = []
+        lookup = Counter(s)
+        max_heap = []
+        for char,cnt in lookup.items():
+            heapq.heappush(max_heap, [-cnt, char])
+        while max_heap:
+            count,char = heapq.heappop(max_heap)
+            while count < 0:
+                result.append(char)
+                count +=1
+        return "".join(result)
+        
+            
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#         hm = Counter(s)
+#         maxHeap = []
+#         res = []
+#         for key, value in hm.items():
+#             heapq.heappush(maxHeap, [-value, key])
+#         while maxHeap:
+#             c = heapq.heappop(maxHeap)
+#             while c[0] < 0:
+#                 res.append(c[1])
+#                 c[0] += 1
+#         return "".join(res)
         
         
         
