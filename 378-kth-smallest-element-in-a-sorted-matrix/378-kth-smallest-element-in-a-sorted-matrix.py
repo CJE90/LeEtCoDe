56741b9f66232113c94363f9b7040d5fb84a5e68
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-03-10 20:39:44 -0500

    Time: 255 ms (61.49%), Space: 18.7 MB (85.50%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
@@ -1,14 +1,80 @@
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        maxHeap = []
-        n = len(matrix)
-        for r in range(min(k,n)):
-            heapq.heappush(maxHeap, (matrix[r][0], r, 0))
-        while k >0:
-            element, r, c = heapq.heappop(maxHeap)
-            if c+1<len(matrix[0]):
-                heapq.heappush(maxHeap, (matrix[r][c+1], r, c+1))
+        minHeap = []
+        
+        for i in range(min(k, len(matrix))):
+            heapq.heappush(minHeap, (matrix[i][0], i, 0))
+        while k > 0:
+            minElement, row, column = heapq.heappop(minHeap)
             
-            k-=1
-        return element
+            if column+1 < len(matrix[0]):
+                heapq.heappush(minHeap, (matrix[row][column+1], row, column+1))
+            k -= 1
+        return minElement
+        
+        
+        
+        
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#         maxHeap = []
+#         n = len(matrix)
+#         for r in range(min(k,n)):
+#             heapq.heappush(maxHeap, (matrix[r][0], r, 0))
+#         while k >0:
+#             element, r, c = heapq.heappop(maxHeap)
+#             if c+1<len(matrix[0]):
+#                 heapq.heappush(maxHeap, (matrix[r][c+1], r, c+1))
+#             k-=1
+#         return element
         
