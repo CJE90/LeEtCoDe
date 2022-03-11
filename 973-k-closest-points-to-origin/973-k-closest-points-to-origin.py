7860f4943aeb531d497461ff21bef7e8f9a03d07
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-03-10 21:59:24 -0500

    Time: 936 ms (70.30%), Space: 20.4 MB (51.41%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -1,20 +1,78 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        maxHeap = []
         result = []
-        
-        def distance(x,y):
-            return math.sqrt(x*x+y*y)
-        
-        
-        for x,y in points:
-            d = distance(x,y)
-            heapq.heappush(maxHeap, (-d, x, y))
-            if len(maxHeap) > k:
-                heapq.heappop(maxHeap)
-        while maxHeap or len(result) < k:
-            _, x, y = heapq.heappop(maxHeap)
+        min_heap = []
+        for point in points:
+            heapq.heappush(min_heap, (self.distance(point[0],point[1]), point[0],point[1]))
+        while k > 0:
+            d, x, y = heapq.heappop(min_heap)
             result.append([x,y])
+            k -= 1
         return result
+            
+    def distance(self, x, y):
+        return math.sqrt(x*x+y*y)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         maxHeap = []
+#         result = []
+        
+#         def distance(x,y):
+#             return math.sqrt(x*x+y*y)
+        
+        
+#         for x,y in points:
+#             d = distance(x,y)
+#             heapq.heappush(maxHeap, (-d, x, y))
+#             if len(maxHeap) > k:
+#                 heapq.heappop(maxHeap)
+#         while maxHeap or len(result) < k:
+#             _, x, y = heapq.heappop(maxHeap)
+#             result.append([x,y])
+#         return result
         
         
