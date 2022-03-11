118fbc0677fc436a478db13b47c7c7d31ac4dd83
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-03-10 20:12:26 -0500

    Time: 151 ms (5.27%), Space: 14.5 MB (83.51%) - LeetHub

--- 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.py
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.py
@@ -6,14 +6,15 @@
 #         self.right = right
 class Solution:
     def delNodes(self, root: Optional[TreeNode], to_delete: List[int]) -> List[TreeNode]:
+        
         result = []
         
-        def deleteNodes(node):
+        def exploreAndPrune(node):
             if not node:
                 return None
             
-            node.left = deleteNodes(node.left)
-            node.right = deleteNodes(node.right)
+            node.left = exploreAndPrune(node.left)
+            node.right = exploreAndPrune(node.right)
             
             if node.val in to_delete:
                 if node.left:
@@ -21,11 +22,72 @@ class Solution:
                 if node.right:
                     result.append(node.right)
                 return None
-            
             return node
         
-        deleteNodes(root)
+        exploreAndPrune(root)
         if root.val not in to_delete:
             result.append(root)
         return result
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+#         result = []
+        
+#         def deleteNodes(node):
+#             if not node:
+#                 return None
+            
+#             node.left = deleteNodes(node.left)
+#             node.right = deleteNodes(node.right)
+            
+#             if node.val in to_delete:
+#                 if node.left:
+#                     result.append(node.left)
+#                 if node.right:
+#                     result.append(node.right)
+#                 return None
+            
+#             return node
+        
+#         deleteNodes(root)
+#         if root.val not in to_delete:
+#             result.append(root)
+#         return result
         
