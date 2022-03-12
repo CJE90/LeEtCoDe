ea5be91575596350a4873632acbc8fb6934122b0
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-03-12 13:29:35 -0500

    Time: 104 ms (86.97%), Space: 18.6 MB (30.63%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -3,7 +3,5 @@ class Solution:
         lookup = defaultdict(list)
         for word in strs:
             lookup[tuple(sorted(word))].append(word)
-        result = []
-        for item in lookup.values():
-            result.append(item)
-        return result
+        
+        return lookup.values()
