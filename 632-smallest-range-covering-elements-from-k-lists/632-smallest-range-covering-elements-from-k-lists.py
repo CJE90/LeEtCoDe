6b3291f181c98fa5aa93ecd2aaf8272b2584e07a
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-03-11 21:27:33 -0500

    Time: 298 ms (65.89%), Space: 20.4 MB (59.66%) - LeetHub

--- 632-smallest-range-covering-elements-from-k-lists/632-smallest-range-covering-elements-from-k-lists.py
+++ 632-smallest-range-covering-elements-from-k-lists/632-smallest-range-covering-elements-from-k-lists.py
@@ -1,19 +1,88 @@
 class Solution:
     def smallestRange(self, nums: List[List[int]]) -> List[int]:
-        min_heap=[(List[0], index, 0) for index,List in enumerate(nums)]
-        heapq.heapify(min_heap)
-        max_val = max(l[0] for l in nums)
-        smallest_range = (-inf,inf)
+        max_val = -inf
+        smallest_range = (-inf, inf)
+        min_heap = []
+         
+        for i in range(len(nums)):
+            heapq.heappush(min_heap, (nums[i][0], i, 0))
+            max_val = max(max_val, nums[i][0])
+        
         while min_heap:
-            min_val, list_index, num_index = heapq.heappop(min_heap)
-            if max_val - min_val < smallest_range[1] - smallest_range[0]:
-                smallest_range = (min_val, max_val)
-                
-            if num_index+1 == len(nums[list_index]):
-                return smallest_range
+            val, i, j = heapq.heappop(min_heap)
+            if max_val - val < smallest_range[1] - smallest_range[0]:
+                smallest_range = (val, max_val)
             
-            next_val = nums[list_index][num_index+1]
-            max_val = max(max_val, next_val)
-            heapq.heappush(min_heap, (next_val, list_index, num_index+1))
-        return smallest_range
+            if j+1 < len(nums[i]):
+                heapq.heappush(min_heap, (nums[i][j+1], i, j+1))
+                max_val = max(max_val, nums[i][j+1])
+            else:
+                return [smallest_range[0], smallest_range[1]]
+        
+        return [smallest_range[0], smallest_range[1]]
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         min_heap=[(List[0], index, 0) for index,List in enumerate(nums)]
+#         heapq.heapify(min_heap)
+#         max_val = max(l[0] for l in nums)
+#         smallest_range = (-inf,inf)
+#         while min_heap:
+#             min_val, list_index, num_index = heapq.heappop(min_heap)
+#             if max_val - min_val < smallest_range[1] - smallest_range[0]:
+#                 smallest_range = (min_val, max_val)
+                
+#             if num_index+1 == len(nums[list_index]):
+#                 return smallest_range
+            
+#             next_val = nums[list_index][num_index+1]
+#             max_val = max(max_val, next_val)
+#             heapq.heappush(min_heap, (next_val, list_index, num_index+1))
+#         return smallest_range
         
