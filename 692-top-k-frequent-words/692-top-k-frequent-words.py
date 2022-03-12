256b961ff43e3a4185ce3b639f6568c5a05ec850
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-03-11 19:09:29 -0500

    Time: 78 ms (55.31%), Space: 13.9 MB (77.05%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.py
+++ 692-top-k-frequent-words/692-top-k-frequent-words.py
@@ -1,14 +1,73 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        maxHeap = []
+        max_heap = []
         result = []
         lookup = Counter(words)
-        for word,frequency in lookup.items():
-            heapq.heappush(maxHeap, (-frequency, word))
-            
-        while maxHeap and len(result) < k:
-            frequency, element = heapq.heappop(maxHeap)
-            
-            result.append(element)
+        for word, cnt in lookup.items():
+            heapq.heappush(max_heap, (-cnt, word))
+        while k > 0:
+            element = heapq.heappop(max_heap)
+            result.append(element[1])
+            k -= 1
         return result
+        
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         maxHeap = []
+#         result = []
+#         lookup = Counter(words)
+#         for word,frequency in lookup.items():
+#             heapq.heappush(maxHeap, (-frequency, word))
+            
+#         while maxHeap and len(result) < k:
+#             frequency, element = heapq.heappop(maxHeap)
+#             result.append(element)
+#         return result
         
