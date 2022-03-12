98c2590566d0fc911bba44961b914066f5e7abdc
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-03-11 20:51:53 -0500

    Time: 36 ms (86.43%), Space: 13.9 MB (48.24%) - LeetHub

--- 767-reorganize-string/767-reorganize-string.py
+++ 767-reorganize-string/767-reorganize-string.py
@@ -1,29 +1,99 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        lookup = Counter(s)
-        que = deque()
-        maxHeap = []
-        result = []
-        time = 0
-        for letter,frequency in lookup.items():
-            heapq.heappush(maxHeap, (-frequency, letter))
         
-        while maxHeap or que:
-            time +=1
-            if maxHeap:
-                frequency,letter = heapq.heappop(maxHeap)
-                if result and result[len(result)-1] == letter:
+        lookup = Counter(s)
+        max_heap = []
+        que = deque()
+        result = []
+        step = 0
+        for character,count in lookup.items():
+            heapq.heappush(max_heap, (-count, character))
+        while max_heap or que:
+            step += 1
+            if max_heap:
+                count,character = heapq.heappop(max_heap)
+                if result and result[-1] == character:
                     return ""
-                result.append(letter)
-                frequency += 1
-                if frequency:
-                    que.append((frequency, letter, time+1))
-                    
-            if que and que[0][2] == time:
-                frequency,letter, _ = que.popleft()
-                heapq.heappush(maxHeap, (frequency, letter))
-                
+                result.append(character)
+                if count+1 < 0:
+                    que.append((count+1, character, step+1))
+            
+            if que:
+                if que[0][2] == step:
+                    count, character, _  = que.popleft()
+                    heapq.heappush(max_heap, (count, character))
         return "".join(result)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         lookup = Counter(s) #O(n)
+#         que = deque()
+#         maxHeap = []
+#         result = []
+#         time = 0
+#         for letter,frequency in lookup.items(): #O(n)
+#             heapq.heappush(maxHeap, (-frequency, letter))
+        
+#         while maxHeap or que:#O(n)
+#             time +=1
+#             if maxHeap:
+#                 frequency,letter = heapq.heappop(maxHeap)
+#                 if result and result[len(result)-1] == letter:
+#                     return ""
+#                 result.append(letter)
+#                 frequency += 1
+#                 if frequency:
+#                     que.append((frequency, letter, time+1))
+                    
+#             if que and que[0][2] == time:
+#                 frequency,letter, _ = que.popleft()
+#                 heapq.heappush(maxHeap, (frequency, letter))
+                
+#         return "".join(result)
                 
                     
         
