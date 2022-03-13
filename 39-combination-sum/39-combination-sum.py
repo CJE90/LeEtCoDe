c219543b82583edad93c8fe6df82863c54fbf769
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-03-12 20:01:50 -0500

    Time: 122 ms (44.03%), Space: 13.9 MB (68.90%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.py
@@ -0,0 +1,19 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        result = []
+        
+        def explore(index, path, target):
+            if target == 0:
+                result.append(path[:])
+                return
+            if target < 0:
+                return 
+            for i in range(index, len(candidates)):
+                path.append(candidates[i])
+                target -= candidates[i]
+                explore(i, path, target)
+                target += candidates[i]
+                path.pop()
+        
+        explore(0, [], target)
+        return result
