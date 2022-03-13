bbe824d599406e7d7e36a23d8f93ec5c72cc9b21
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-03-13 19:36:43 -0400

    Time: 87 ms (30.63%), Space: 18 MB (88.99%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
@@ -19,6 +19,8 @@ class Solution:
             if k == 0:
                 result = node.val
                 return
+            if k<0:
+                return
             explore(node.right)
             
         explore(root)
