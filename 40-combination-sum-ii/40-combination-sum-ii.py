0155510f98c342864d5538fd55768052376c9bb9
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-03-12 20:35:32 -0500

    Time: 106 ms (46.99%), Space: 13.9 MB (95.45%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        result = []
+        candidates.sort()
+        
+        def explore(index, path, target):
+            if target == 0:
+                result.append(path[:])
+                return
+            if target < 0:
+                return
+            for i in range(index, len(candidates)):
+                if i > index and candidates[i] == candidates[i-1]:
+                   
+                    continue
+                path.append(candidates[i])
+                target -= candidates[i]
+                explore(i+1, path, target)
+                target += candidates[i]
+                path.pop()
+                        
+        explore(0, [], target)
+        return result
+        
