1706c9661a6f60ccc318868012d6a34dea47ba2e
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-03-14 19:21:12 -0400

    Time: 100 ms (82.59%), Space: 17.7 MB (6.47%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.py
+++ 261-graph-valid-tree/261-graph-valid-tree.py
@@ -1,37 +1,24 @@
+from collections import defaultdict
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        if n == 0:
-            return True
-        visited = set()
-        
         adjList = defaultdict(list)
-        for node1, node2 in edges:
-            adjList[node1].append(node2)
-            adjList[node2].append(node1)
-            
-        stack = [(0,-1)]
-        while stack:
-            curNode, prevNode = stack.pop()
-            if curNode in visited:
+        for start,end in edges:
+            adjList[start].append(end)
+            adjList[end].append(start)
+        visited  = set()
+        
+        def explore(node,prev): 
+            # if node == prev:
+            #     return True
+            if node in visited:
                 return False
-            visited.add(curNode)
-            for child in adjList[curNode]:
-                if child == prevNode:
-                    continue
-                stack.append((child, curNode))
-        return len(visited) == n and True
-            
-#         def dfs(node, prev):
-#             if node == prev:
-#                 return True
-#             if node in visited:
-#                 return False
-#             visited.add(node)
-#             for child in adjList[node]:
-#                 if child != prev:
-#                     if not dfs(child, node):
-#                         return False
-#             return True
-#         return dfs(0, -1) and len(visited) == n
-            
-            
+            visited.add(node)
+            for child in adjList[node]:
+                if child != prev:
+                    if not explore(child, node):
+                        return False
+            return True
+        
+        return explore(0,-1) and n == len(visited)
+        
+        
