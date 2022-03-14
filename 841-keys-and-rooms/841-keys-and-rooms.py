010c699b90b57fbd4a66bc396152d7386c09bc39
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-03-14 17:34:05 -0400

    Time: 143 ms (7.20%), Space: 14.9 MB (18.72%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.py
+++ 841-keys-and-rooms/841-keys-and-rooms.py
@@ -1,31 +1,14 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
         visited = set()
-        que = collections.deque()
-        que.append(0)
-        visited.add(0)
         
-        while que:
-            room = que.popleft()
+        
+        def explore(room):
+            visited.add(room)
             for key in rooms[room]:
                 if key not in visited:
-                    visited.add(key)
-                    que.append(key)
-        return len(rooms) == len(visited)
+                    explore(key)
         
-#         if not rooms:
-#             return True
-#         visited = set()
-#         def exploreRooms(room):
-#             visited.add(room)
-#             for key in rooms[room]:
-#                 if key not in visited:
-#                     exploreRooms(key)
-#         exploreRooms(0)
         
-#         return len(rooms) == len(visited)
-    #Time O(n): May need to explore each room
-    #Space O(n): may need recursive stack call hold each room until the end
-    
-    
-        
+        explore(0)
+        return len(visited) == len(rooms)
