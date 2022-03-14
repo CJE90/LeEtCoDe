35e0d65435704aa929a1fbc4e667220dd78bc602
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-03-14 19:58:49 -0400

    Time: 128 ms (63.66%), Space: 17.2 MB (21.58%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,35 +1,31 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        if numCourses == 0:
-            return False
+        adjList = defaultdict(list)
         visited = set()
         visiting = set()
-        adjList = defaultdict(list)
-        
-        for course, prereq in prerequisites:
-            adjList[course].append(prereq)
+        for course,preReq in prerequisites:
+            adjList[course].append(preReq)
             
-        def dfs(node):
+        def explore(node):
             if node in visiting:
                 return False
-            if node not in adjList:
-                return True
+            # if node not in adjList:
+            #     return True
             visiting.add(node)
-            for child in adjList[node]:
-                if not dfs(child):
+            
+            for preReq in adjList[node]:
+                if not explore(preReq):
                     return False
-            visited.add(node)
+            #visited.add(node)
             visiting.remove(node)
             adjList[node] = []
             return True
-            
+        
         
         for i in range(numCourses):
             if i not in visited:
-                if not dfs(i):
+                if not explore(i):
                     return False
         return True
         
-            
         
-       
