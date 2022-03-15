16adfbd15c6ad96b69e6271d4086c62ae3d15e14
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-03-14 22:16:21 -0400

    Time: 40 ms (75.91%), Space: 13.9 MB (72.03%) - LeetHub

--- 269-alien-dictionary/269-alien-dictionary.py
+++ 269-alien-dictionary/269-alien-dictionary.py
@@ -9,28 +9,27 @@ class Solution:
             if len(word1) > len(word2) and word2 == word1[: len(word2)]:
                     return ""
             for j in range(len(word1)):
+                
                 if word1[j] == word2[j]:
                     continue
-                
                 if word2[j] not in adjList[word1[j]]:
-                    inDegree[word2[j]] +=1
+                    inDegree[word2[j]] += 1
                 adjList[word1[j]].add(word2[j])
                 break
+                    
         que = deque()
         for c in inDegree:
             if inDegree[c] == 0:
                 que.append(c)
         while que:
-            c = que.popleft()
-            result.append(c)
-            for child in adjList[c]:
-                inDegree[child] -= 1
-                if inDegree[child] == 0:
-                    que.append(child)
+            char = que.popleft()
+            result.append(char)
+            for l in adjList[char]:
+                inDegree[l] -= 1
+                if inDegree[l] == 0:
+                    que.append(l)
         res = "".join(result)
         if len(res) == len(inDegree):
             return res
         return ""
-        
 
-        
