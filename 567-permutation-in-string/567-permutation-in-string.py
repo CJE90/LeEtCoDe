786d3f81455771a525c9476b19bb61f4d0d8448c
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-03-21 15:42:14 -0400

    Time: 108 ms (63.68%), Space: 13.9 MB (73.56%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -2,22 +2,34 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1) > len(s2):
             return False
+        if s1 == s2:
+            return True
+        
+        l = 0
         lookup = Counter(s1)
         need = len(s1)
-        l = 0
+        
+        
         for r in range(len(s2)):
             if s2[r] in lookup:
+                
                 if lookup[s2[r]] > 0:
                     need -= 1
+                    
                 lookup[s2[r]] -= 1
+                
             if r-l+1 > len(s1):
+                
                 if s2[l] in lookup:
                     lookup[s2[l]] += 1
                     if lookup[s2[l]] > 0:
-                        need +=1
-                l += 1
+                        need += 1
+                l+=1
+                        
             
             if need == 0:
                 return True
         return False
+                
+          
         
