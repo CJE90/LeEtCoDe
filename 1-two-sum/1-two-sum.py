9c82f6152a5e1c1e720e9521777836c8a1ce26d5
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-05-18 17:21:15 -0400

    Time: 64 ms (88.60%), Space: 15.5 MB (9.32%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,10 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        lookup = defaultdict(int)
-        for i,v in enumerate(nums):
-            compliment = target-v
-            if compliment in lookup:
-                return [i,lookup[compliment]]
-            lookup[v] = i
-        return [-1,-1]
+        lookup = {}
+        result = []
+        for index,value in enumerate(nums):
+            if value in lookup:
+                result.append(lookup[value])
+                result.append(index)
+                return result
+            lookup[target-value] = index
+        return []
         
