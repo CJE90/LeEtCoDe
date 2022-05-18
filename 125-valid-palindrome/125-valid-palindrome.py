6ceceab9f6356dfd099391aa03c2e258d325d11f
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-05-18 18:16:25 -0400

    Time: 91 ms (19.61%), Space: 14.8 MB (37.38%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,18 +1,15 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s2 = filter(lambda x:x.isalnum(),s)
-        res = list(s2)
+        res = list(filter(lambda x:x.isalnum(), s))
         l = 0
         r = len(res)-1
-        
-        while l < r:
+        while l<r:
             if res[l].isalpha() and res[r].isalpha():
                 if res[l].lower() != res[r].lower():
                     return False
-            elif res[l] != res[r]:
+            elif res[l].lower() != res[r].lower():
                 return False
-            
-            l+= 1
-            r-=1
+            l += 1
+            r -= 1
         return True
         
