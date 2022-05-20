a4036b5eba4e618c9afbfa08bef0abb8b3498a15
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-05-19 21:15:17 -0400

    Time: 601 ms (53.26%), Space: 14.9 MB (79.89%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
@@ -1,23 +1,25 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
-        maxPath = 0
-        dp = [[0 for j in range(len(matrix[0]))] for i in range(len(matrix))]
+        R = len(matrix)
+        C = len(matrix[0])
+        directions = [(1,0),(-1,0), (0,1),(0,-1)]
+        dp = [[None] * C for _ in range(R)]
         
         
-        def explore(r,c):
-            if dp[r][c] > 0:
-                return dp[r][c]
-            
-            for newRow, newCol in [[r+1,c],[r-1,c],[r,c+1],[r,c-1]]:
-                if 0 <= newRow < len(matrix) and 0<= newCol < len(matrix[0]) and matrix[newRow][newCol]>matrix[r][c]:
-                    dp[r][c] = max(dp[r][c], explore(newRow,newCol))
-            dp[r][c] += 1
-            return dp[r][c]
-               
-        
-        for r in range(len(matrix)):
-            for c in range(len(matrix[0])):
-                maxPath = max(maxPath, explore(r,c))
-        print(dp)
-        return maxPath
-    
+        def longestPath(x, y):
+            if dp[x][y]:
+                return dp[x][y]
+            best = 1
+            for dx, dy in directions:
+                nx = x+dx
+                ny = y+dy
+                
+                if 0 <= nx < R and 0 <= ny < C and matrix[x][y] < matrix[nx][ny]:
+                    best = max(best, longestPath(nx,ny)+1)
+            dp[x][y] = best       
+            return best
+        long = 0
+        for r in range(R):
+            for c in range(C):
+                long = max(long, longestPath(r,c))
+        return long
