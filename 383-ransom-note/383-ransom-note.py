5209451ed53df52426bad609ff9b030ff02f190f
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-05-23 19:41:38 -0400

    Time: 58 ms (80.04%), Space: 14.1 MB (53.63%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -5,7 +5,7 @@ class Solution:
         note_lookup = Counter(ransomNote)
         mag_lookup = Counter(magazine)
         for char, count in note_lookup.items():
-            if char not in mag_lookup or count > mag_lookup[char]:
+            if count > mag_lookup[char]:
                 return False
         return True
         
