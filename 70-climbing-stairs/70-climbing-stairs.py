b93017e9517a146ce8b7927703263939c3f0d5ee
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-05-24 17:08:57 -0400

    Time: 35 ms (71.26%), Space: 13.9 MB (12.16%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,9 +1,7 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         dp = [-1] * (n + 1)
-        dp[1] = 1
-        if n > 1:
-            dp[2] = 2
+        
         self.helper(n, dp)
         return dp[-1]
     
@@ -12,24 +10,12 @@ class Solution:
             return dp[n]
         if n < 0:
             return 0
+        if n == 1:
+            dp[1] = 1
+            return dp[1]
+        if n == 2:
+            dp[2] = 2
+            return dp[2]
         
         dp[n] = self.helper(n-1,dp)+self.helper(n-2,dp)
         return dp[n]
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # 1: 1
-        # 2: 1+1, 2
-        # 3: 1+1+1, 2+1
-        # 4: 1+1+1+1, 2+1+1, 2+2
-        # 5: 1+1+1+1+1+1, 2+1+1+1, 2+2+1
