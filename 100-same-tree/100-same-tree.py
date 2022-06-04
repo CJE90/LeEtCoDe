951d86b026db5e0874d6ded8cf28b36700d66d65
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-06-03 20:41:26 -0400

    Time: 42 ms (51.48%), Space: 14.1 MB (28.94%) - LeetHub

--- 100-same-tree/100-same-tree.py
+++ 100-same-tree/100-same-tree.py
@@ -6,16 +6,14 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        queue = [(p, q)]
-        while queue:
-            node1, node2 = queue.pop(0)
-            if not node1 and not node2:
-                continue
-            elif None in [node1, node2]:
-                return False
-            else:
-                if node1.val != node2.val:
-                    return False
-                queue.append((node1.left, node2.left))
-                queue.append((node1.right, node2.right))
-        return True
+        if not p and not q:
+            return True
+        if not p:
+            return False
+        if not q:
+            return False
+        if p.val != q.val:
+            return False
+        left = self.isSameTree(p.left, q.left)
+        right = self.isSameTree(p.right, q.right)
+        return left and right
