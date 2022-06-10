2847b1bbba00d6a57dc2df9534c4a98ee7c8e7d2
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-06-10 18:35:37 -0400

    Time: 39 ms (76.60%), Space: 13.9 MB (59.08%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.py
+++ 101-symmetric-tree/101-symmetric-tree.py
@@ -6,22 +6,19 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        if not root:
-            return False
-        return self.isMirror(root.left, root.right)
-    
-    def isMirror(self, left, right):
-        if not left and not right:
-            return True
-        if not left or not right:
-            return False
         
-        if left.val == right.val:
-            l = self.isMirror(left.left, right.right)
-            r = self.isMirror(left.right, right.left)
-            return l and r
-        else: return False
+        def is_mirror(left, right):
+            if not left and not right:
+                return True
+            if not left or not right:
+                return False
+            if left.val == right.val:
+                left_mirror = is_mirror(left.left, right. right)
+                right_mirror = is_mirror(left.right, right.left)
+                return left_mirror and right_mirror
+            else:
+                return False
+            
         
-    
-    
+        return is_mirror(root.left, root.right)
     
