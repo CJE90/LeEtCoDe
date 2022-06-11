b2f5ff26cf89296371e3f5bd81560ae6da9846b5
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-06-11 13:39:33 -0400

    Time: 1514 ms (12.01%), Space: 20.4 MB (37.26%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -6,9 +6,7 @@ class Solution:
             heapq.heappush(max_heap, (self.distance(p)*-1, p))
             if len(max_heap) > k:
                 heapq.heappop(max_heap)
-        for val in max_heap:
-            result.append(val[1])
-        return result
+        return [points[1] for points in max_heap]
                 
     def distance(self, point):
         return sqrt(point[0]*point[0]+point[1]*point[1])
