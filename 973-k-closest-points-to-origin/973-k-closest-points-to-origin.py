a032f2b83ac7995d6abe2481f0ef52659d7c9ae6
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-06-11 13:21:58 -0400

    Time: 815 ms (91.59%), Space: 20.2 MB (79.49%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -1,78 +1,9 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        result = []
-        min_heap = []
-        for point in points:
-            heapq.heappush(min_heap, (self.distance(point[0],point[1]), point[0],point[1]))
-        while k > 0:
-            d, x, y = heapq.heappop(min_heap)
-            result.append([x,y])
-            k -= 1
-        return result
-            
-    def distance(self, x, y):
-        return math.sqrt(x*x+y*y)
         
+        points.sort(key = lambda x: self.distance(x))
+        return points[0:k]
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         maxHeap = []
-#         result = []
-        
-#         def distance(x,y):
-#             return math.sqrt(x*x+y*y)
-        
-        
-#         for x,y in points:
-#             d = distance(x,y)
-#             heapq.heappush(maxHeap, (-d, x, y))
-#             if len(maxHeap) > k:
-#                 heapq.heappop(maxHeap)
-#         while maxHeap or len(result) < k:
-#             _, x, y = heapq.heappop(maxHeap)
-#             result.append([x,y])
-#         return result
-        
+    def distance(self, point):
+        return sqrt(point[0]*point[0]+point[1]*point[1])
         
