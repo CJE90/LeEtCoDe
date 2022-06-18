8cc5242303a9f8dedf98cf9431bf667de836aac7
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-06-18 14:47:41 -0400

    Time: 133 ms (59.56%), Space: 17.4 MB (15.15%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,49 +1,52 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        visited = set()
         adj_list = collections.defaultdict(list)
         for course, pre_req in prerequisites:
             adj_list[pre_req].append(course)
-        
-        visited = set()
-        visiting = set()
-        def explore(node):
-            if node in visiting:
-                return False
-            visiting.add(node)
-            for n in adj_list[node]:
-                if not explore(n):
-                    return False
-            visiting.remove(node)
+            
+        def cycle(node, tracker, visited):
             visited.add(node)
-            adj_list[node] = []
-            return True
-        
-        for n in range(numCourses):
-            if n not in visited:
-                if not explore(n):
-                    return False
-        return True
+            tracker[node] = True
+            for n in adj_list[node]:
+                if (n not in visited and cycle(n, tracker, visited)) or n in tracker:
+                    return True
+                # elif n in tracker:
+                #     return True
+            del tracker[node]
             
-#         visited = set()
-#         for course, pre_req in prerequisites:
-#             adj_list[pre_req].append(course)
-            
-#         def cycle(node, tracker, visited):
-#             visited.add(node)
-#             tracker[node] = True
-#             for n in adj_list[node]:
-#                 if n not in visited and cycle(n, tracker, visited):
-#                     return True
-#                 elif n in tracker:
-#                     return True
-#             del tracker[node]
-            
-#             return False
+            return False
            
         
-#         for i in range(numCourses):
-#             tracker = {}
-#             if i not in visited and cycle(i, tracker, visited):
+        for i in range(numCourses):
+            tracker = {}
+            if i not in visited and cycle(i, tracker, visited):
+                return False
+        return True
+        
+#         adj_list = collections.defaultdict(list)
+#         for course, pre_req in prerequisites:
+#             adj_list[pre_req].append(course)
+        
+#         visited = []
+#         visiting = set()
+#         def explore(node):
+#             if node in visiting:
 #                 return False
+#             visiting.add(node)
+#             for n in adj_list[node]:
+#                 if not explore(n):
+#                     return False
+#             visiting.remove(node)
+#             visited.append(node)
+#             adj_list[node] = []
+#             return True
+        
+#         for n in range(numCourses):
+#             if not explore(n):
+#                 return False
+#         print(visited)
 #         return True
+            
+
         
