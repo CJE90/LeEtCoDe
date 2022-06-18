ed4cb33d5d8110c81367987d11ef9632fe28ba78
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-06-18 13:21:43 -0400

    Time: 128 ms (63.67%), Space: 17.3 MB (15.15%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,26 +1,47 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         adj_list = collections.defaultdict(list)
-        visited = set()
         for course, pre_req in prerequisites:
             adj_list[pre_req].append(course)
-            
-        def cycle(node, tracker, visited):
-            visited.add(node)
-            tracker[node] = True
+        visited = set()
+        visiting = set()
+        def explore(node):
+            if node in visiting:
+                return False
+            visiting.add(node)
             for n in adj_list[node]:
-                if n not in visited and cycle(n, tracker, visited):
-                    return True
-                elif n in tracker:
-                    return True
-            del tracker[node]
-            
-            return False
-           
+                if not explore(n):
+                    return False
+            visiting.remove(node)
+            visited.add(node)
+            adj_list[node] = []
+            return True
         
-        for i in range(numCourses):
-            tracker = {}
-            if i not in visited and cycle(i, tracker, visited):
+        for n in range(numCourses):
+            if not explore(n):
                 return False
         return True
+            
+#         visited = set()
+#         for course, pre_req in prerequisites:
+#             adj_list[pre_req].append(course)
+            
+#         def cycle(node, tracker, visited):
+#             visited.add(node)
+#             tracker[node] = True
+#             for n in adj_list[node]:
+#                 if n not in visited and cycle(n, tracker, visited):
+#                     return True
+#                 elif n in tracker:
+#                     return True
+#             del tracker[node]
+            
+#             return False
+           
+        
+#         for i in range(numCourses):
+#             tracker = {}
+#             if i not in visited and cycle(i, tracker, visited):
+#                 return False
+#         return True
         
