b92cd24e047b053ed4b00b8122f6e06e1418e288
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-09-18 21:17:45 -0400

    Time: 738 ms (79.75%), Space: 14.6 MB (62.19%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -3,17 +3,16 @@ class Solution:
         left = 0
         max_window = 0
         ones_count = 0
-        lookup = defaultdict(int)
+        
         for right in range(len(nums)):
             if nums[right] == 1:
                 ones_count += 1
-            lookup[nums[right]] += 1
+            
             if right-left+1 - ones_count > k:
-                lookup[nums[left]] -= 1
+                
                 if nums[left] == 1:
                     ones_count -= 1
-                if lookup[nums[left]] < 1:
-                    del lookup[nums[left]]
+                
                 left += 1
             max_window = max(max_window, right-left+1)
         return max_window
