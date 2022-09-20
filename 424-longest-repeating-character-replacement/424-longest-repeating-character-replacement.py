2badee9e03082ab44be058205746ddca448e108f
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-09-19 21:37:19 -0400

    Time: 210 ms (57.68%), Space: 13.9 MB (57.11%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -2,16 +2,17 @@ class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         left = 0
         max_window = 0
-        max_char = 0
         lookup = defaultdict(int)
+        max_char = 0
         for right in range(len(s)):
             lookup[s[right]] += 1
             max_char = max(max_char, lookup[s[right]])
-            if (right-left+1 - max_char) > k:
+            if right-left+1 - max_char > k:
                 lookup[s[left]] -= 1
                 if lookup[s[left]] < 1:
                     del lookup[s[left]]
                 left += 1
             max_window = max(max_window, right-left+1)
         return max_window
+                
         
