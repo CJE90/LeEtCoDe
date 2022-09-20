5d182435365d2619d6c46490ee5ea6d1f3974de8
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-09-19 21:32:27 -0400

    Time: 1409 ms (21.21%), Space: 14.6 MB (94.83%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -3,16 +3,13 @@ class Solution:
         left = 0
         max_window = 0
         ones_count = 0
-        
         for right in range(len(nums)):
             if nums[right] == 1:
                 ones_count += 1
-            
             if right-left+1 - ones_count > k:
-                
                 if nums[left] == 1:
                     ones_count -= 1
-                
                 left += 1
-            max_window = max(max_window, right-left+1)
+            max_window = max(max_window, right-left +1)
         return max_window
+        
