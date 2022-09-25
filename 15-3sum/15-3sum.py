35d7316d27c001cb6b6d8cfbcc189a4327fa1073
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-09-25 10:04:20 -0400

    Time: 1782 ms (44.69%), Space: 17.8 MB (85.23%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,30 +1,27 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # X + Y + Z == 0
-        # Y + Z = -X
-        result = []
+        triplets = []
         nums.sort()
-        for i in range(len(nums)-2):
+        
+        for i in range(len(nums)):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
-            self.search_for_trips(nums, -nums[i], i+1, result)
-        return result
-    def search_for_trips(self, arr, target_sum, left, result):
+            self.search_pairs(nums, -nums[i], i+1, triplets)
+        return triplets
+    def search_pairs(self, arr, target, left, triplets):
         right = len(arr)-1
         while left < right:
-            _sum = arr[left]+arr[right]
-            if _sum == target_sum:
-                result.append([-target_sum, arr[left], arr[right]])
+            current_sum = arr[left] + arr[right]
+            if current_sum == target:
+                triplets.append([-target, arr[left], arr[right]])
                 left += 1
                 right -= 1
-                while left < right and arr[left] == arr[left-1]:                    
-                        left += 1
+                while left < right and arr[left] == arr[left-1]:
+                    left +=1
                 while left < right and arr[right] == arr[right+1]:
-                        right -= 1
-            elif _sum < target_sum:
+                    right -= 1
+            elif current_sum < target:
                 left += 1
             else:
                 right -= 1
-        
-                
         
