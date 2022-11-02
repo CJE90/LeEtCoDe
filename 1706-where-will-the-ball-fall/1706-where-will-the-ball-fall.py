ccb0ca77c8cdb3ca3dbe3db26dc0b72f3b3a8b44
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-11-01 22:00:49 -0400

    Time: 503 ms (37.09%), Space: 14.1 MB (98.50%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
@@ -1,19 +1,37 @@
 class Solution:
     def findBall(self, grid: List[List[int]]) -> List[int]:
-        result = []
-        number_of_balls = len(grid[0])
-        for i in range(number_of_balls):
-            result.append(self.ball_can_fall(0,i,grid))
+        result = [0] * len(grid[0])
+        
+        for col in range(len(grid[0])):
+            current_col = col
+            for row in range(len(grid)):
+                next_col = current_col + grid[row][current_col]
+                if next_col < 0 or next_col >= len(grid[0]) or grid[row][next_col] != grid[row][current_col]:
+                    result[col] = -1
+                    break
+                result[col] = next_col
+                current_col = next_col
         return result
-    def ball_can_fall(self, row, col, grid):
-        # We have exited the grid
-        if row == len(grid):
-            return col
-        # We have hit a wall
-        if col < 0 or col >= len(grid[0]):
-            return -1
-        if grid[row][col] == 1 and col+1 < len(grid[0]) and grid[row][col+1] == 1:
-            return self.ball_can_fall(row+1, col+1, grid)
-        elif grid[row][col] == -1 and col-1 >= 0 and grid[row][col-1] == -1:
-            return self.ball_can_fall(row+1, col-1, grid)
-        return -1
+            
+    
+    
+    
+    
+    #     result = []
+    #     number_of_balls = len(grid[0])
+    #     for i in range(number_of_balls):
+    #         result.append(self.ball_can_fall(0,i,grid))
+    #     return result
+    # def ball_can_fall(self, row, col, grid):
+    #     # We have exited the grid
+    #     if row == len(grid):
+    #         return col
+    #     # We have hit a wall
+    #     if col < 0 or col >= len(grid[0]):
+    #         return -1
+    #     # The ball will not roll out of bounds and can continue
+    #     if grid[row][col] == 1 and col+1 < len(grid[0]) and grid[row][col+1] == 1:
+    #         return self.ball_can_fall(row+1, col+1, grid)
+    #     elif grid[row][col] == -1 and col-1 >= 0 and grid[row][col-1] == -1:
+    #         return self.ball_can_fall(row+1, col-1, grid)
+    #     return -1
