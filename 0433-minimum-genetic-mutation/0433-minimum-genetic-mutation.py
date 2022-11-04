bd361210d854b49ca13b46efe31cba976118b25f
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-11-03 22:27:45 -0400

    Time: 34 ms (90.23%), Space: 13.9 MB (86.75%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
@@ -1,25 +1,19 @@
 class Solution:
     def minMutation(self, start: str, end: str, bank: List[str]) -> int:
-        seen = set()
-        queue = deque()
-        # add our first node into the queue and add it to our seen set
-        # seen will prevent us from visiting the same node twice
-        queue.append([start,0])
-        seen.add(start)
+        seen = set(start)
+        que = deque()
+        que.append([start,0])
         
-        while queue:
-            node, steps = queue.popleft()
+        while que:
+            node, steps = que.popleft()
             if node == end:
                 return steps
-            
-            for c in 'ACGT':
+                        
+            for c in 'ACTG':
                 for i in range(len(node)):
                     neighbor = node[:i] + c + node[i+1:]
-                    #AACCGGTT
                     if neighbor not in seen and neighbor in bank:
-                        queue.append((neighbor, steps+1))
+                        que.append([neighbor, steps+1])
                         seen.add(neighbor)
+                        
         return -1
-                    
-        
-        
