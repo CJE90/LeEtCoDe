9a14ee2801377cd930468b1d45731f244324dc48
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-11-06 19:51:40 -0500

    Time: 30 ms (95.26%), Space: 13.9 MB (54.69%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.py
+++ 1323-maximum-69-number/1323-maximum-69-number.py
@@ -2,11 +2,11 @@ class Solution:
     def maximum69Number (self, num: int) -> int:
         to_list = []
         while num > 0:
-            to_list.append(num % 10)
+            to_list.append(str(num % 10))
             num = num // 10
         to_list.reverse()
         for i in range(len(to_list)):
-            if to_list[i] == 6:
-                to_list[i] = 9
+            if to_list[i] == '6':
+                to_list[i] = '9'
                 break
-        return int("".join(map(str, to_list)))
+        return int("".join(to_list))
