117fed4df18b0aae24af936781f8fa820f354914
Chris Ellis <78622890+CJE90@users.noreply.github.com>
2022-11-07 20:25:01 -0500

    Time: 77 ms (5.25%), Space: 14.2 MB (47.15%) - LeetHub

---
+++ 0339-nested-list-weight-sum/0339-nested-list-weight-sum.py
@@ -0,0 +1,64 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def depthSum(self, nestedList: List[NestedInteger]) -> int:
+        
+        answer = 0
+        for i in range(len(nestedList)):
+            if nestedList[i].isInteger():
+                answer += nestedList[i].getInteger()
+            else:
+                answer += self.helper(2, nestedList[i].getList())
+        return answer
+        
+    def helper(self, depth, nestedList):
+        answer = 0
+        for i in range(len(nestedList)):
+            item = nestedList[i]
+            if item.isInteger():
+                answer += item.getInteger() * depth
+            else:
+                answer += self.helper(depth+1, item.getList())
+        return answer
+        
